777bca47e048663dc5a76a7e97ffc34024fcd0b5
Kevin Zuber <ukev@knet.eu>
2009-12-04 15:42:36 +0100

    Thumbnail-Anzeige

--- Implementierung/src/org/studien/thumbnail/DemoThumbnail.java
+++ Implementierung/src/org/studien/thumbnail/DemoThumbnail.java
@@ -1,5 +1,7 @@
 package org.studien.thumbnail;
 
+import java.awt.Dimension;
+import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.Toolkit;
 import java.awt.image.BufferedImage;
@@ -7,26 +9,42 @@ import java.io.File;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
+import javax.swing.JComponent;
 
 
-public class DemoThumbnail {
+public class DemoThumbnail extends JComponent {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 403168278892206655L;
+	static BufferedImage image;
 	/**
 	 * @param args
 	 * @throws IOException 
 	 */
 	public static void main(String[] args) throws IOException {
-		BufferedImage image = ImageIO.read( new File("Implementierung/src/org/studien/thumbnail/schwarzwald-Urlaub.jpg") ); 
+		BufferedImage rawImage = ImageIO.read( new File("Implementierung/src/org/studien/thumbnail/schwarzwald-Urlaub.jpg") ); 
 		int percent = 50;
-		Image scaled1 = image.getScaledInstance(
-		  (image.getWidth() * percent) / 100, 
-		  (image.getHeight() * percent) / 100, 
+		BufferedImage thumbnailed = (BufferedImage) rawImage.getScaledInstance(
+		  (rawImage.getWidth() * percent) / 100, 
+		  (rawImage.getHeight() * percent) / 100, 
 		  Image.SCALE_SMOOTH ); 
-		Image scaled2 = image.getScaledInstance( 
-		  Toolkit.getDefaultToolkit().getScreenSize().width, 
-		  Toolkit.getDefaultToolkit().getScreenSize().height, 
-		  Image.SCALE_SMOOTH );
+		setImage(thumbnailed);
 
 	}
-
+	 public static void setImage( BufferedImage imageInput ) 
+	  { 
+	    image = imageInput; 
+	    setPreferredSize( new Dimension(image.getWidth(), image.getHeight()) ); 
+	    repaint(); 
+	    invalidate(); 
+	  } 
+	
+	
+	protected void paintComponent( Graphics g ) 
+	  { 
+	    if ( image != null ) 
+	      g.drawImage( image, 0, 0, this ); 
+	  } 
 }
