10a92d4430ea6c026a36b46000e4bddabc0a620f
Kevin Zuber <uKev@knet.eu>
2009-12-06 18:09:53 +0100

    Performance Messung genauer aufgelöst

--- Implementierung/src/org/studien/thumbnail/DemoThumbnailSpeed.java
+++ Implementierung/src/org/studien/thumbnail/DemoThumbnailSpeed.java
@@ -13,30 +13,68 @@ public class DemoThumbnailSpeed {
 	 * @param args
 	 * @throws IOException 
 	 */
+	static private long zstVorher;
+	static private long zstNachher;
+	static private long zstGesamt = 0;
 	public static void main(String[] args) throws IOException {
-		String tp = DemoThumbnailQuality.tPath;
-		
-		long zstVorher;
-		long zstNachher;
-
+		String tPath = "Implementierung/src/org/studien/thumbnail/";
 		
 		// start time measurement
 		zstVorher = System.currentTimeMillis();
+		measurepoint("Anfang:");
 		
-		BufferedImage rawImage = ImageIO.read( new File(tp + "10MegTestPic.jpg") ); 
+		BufferedImage rawImage = ImageIO.read( new File(tPath + "10MegTestPic.jpg") ); 
+		measurepoint("Eingelesen");
 		// scale and save the thumbnail
-		ImageIO.write(DemoThumbnailQuality.getThumbOf(rawImage, 200, Image.SCALE_FAST), "jpg", new File(tp + "10MegTestPic_thumb_default.jpg"));
+		//ImageIO.write(DemoThumbnailQuality.getThumbOf(rawImage, 200, Image.SCALE_FAST), "jpg", new File(tPath + "10MegTestPic_thumb_default.jpg"));
 		
 		// scale and save the thumbnail in 2 steps
-		//BufferedImage temp = DemoThumbnailQuality.getThumbOf(rawImage, 200, Image.SCALE_FAST);
-		//ImageIO.write(temp, "jpg", new File(tp + "10MegTestPic_thumb_default.jpg"));
 		
+		BufferedImage temp = getThumbOf(rawImage, 200, Image.SCALE_FAST);		
+		
+		ImageIO.write(temp, "jpg", new File(tPath + "10MegTestPic_thumb_default.jpg"));
+		measurepoint("Thumbnail gespeichert");
 
+		measurepoint("Gesamtzeit");
 		// stop time measurement
 		zstNachher = System.currentTimeMillis();
-		System.out.println("Zeit benötigt: " + ((zstNachher - zstVorher)) + " milisec");
-		System.out.println("Zeit benötigt: " + ((zstNachher - zstVorher)/1000) + " sec");
+	}
+	public static BufferedImage getThumbOf(BufferedImage bImage, int maxWidthOrHight, int hints){
+		int width = bImage.getWidth();
+		int height = bImage.getHeight();
+		
+		if (width>=height){
+			width = maxWidthOrHight;
+			height = -1; // negative Höhe bedeutet, dass diese Höhe dem Seitenverhältnis entsprechend an die neue Breite angepasst wird.
+		}else{
+			width = -1; // negative breite: Breite wird dem Seitenverhältnis entsprechend an die neue Höhe angepasst.
+			height = maxWidthOrHight;
+		}
+		measurepoint("Neue höhe und Breite berechnet");
 
+		Image thumbnailed = bImage.getScaledInstance(width, height, hints);
+		measurepoint("Skalierte Instanz zurückgeliefert");
+
+		BufferedImage bThumb = new BufferedImage(thumbnailed.getWidth(null),thumbnailed.getHeight(null), bImage.getType());
+		measurepoint("Buffered Image erzeugt");
+
+		
+		bThumb.createGraphics().drawImage(thumbnailed, 0, 0, null);
+		measurepoint("Skalierte Instanz in Buffered Image geschrieben");
+
+		return bThumb;
+	}
+	static void measurepoint(String comment){
+		zstNachher = System.currentTimeMillis();
+		long zstDiff = zstNachher - zstVorher - zstGesamt;
+		zstGesamt += zstDiff;
+		System.out.println(comment + ":");
+		System.out.println("Zeit benötigt (letzter Schritt): " + ((zstDiff)) + " milisec");
+		System.out.println("Zeit benötigt (letzter Schritt): " + ((zstDiff)/1000) + " sec");
+		System.out.println("Zeit benötigt (gesamt): " + ((zstGesamt)) + " milisec");
+		System.out.println("Zeit benötigt (gesamt): " + ((zstGesamt)/1000) + " sec");
+		System.out.println();
+		zstVorher = System.currentTimeMillis() - zstGesamt;
 	}
 
 }
