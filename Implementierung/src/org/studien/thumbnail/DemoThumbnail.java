0034a64b594dc5bf0567d8e3adae46647560a95c
Kevin Zuber <uKev@knet.eu>
2009-12-06 15:25:34 +0100

    Thumbnail Erstellung funktioniert

--- Implementierung/src/org/studien/thumbnail/DemoThumbnail.java
+++ Implementierung/src/org/studien/thumbnail/DemoThumbnail.java
@@ -1,50 +1,42 @@
 package org.studien.thumbnail;
 
-import java.awt.Dimension;
-import java.awt.Graphics;
 import java.awt.Image;
-import java.awt.Toolkit;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
-import javax.swing.JComponent;
 
 
-public class DemoThumbnail extends JComponent {
-
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 403168278892206655L;
-	static BufferedImage image;
+public class DemoThumbnail{
+	
 	/**
 	 * @param args
 	 * @throws IOException 
 	 */
 	public static void main(String[] args) throws IOException {
-		BufferedImage rawImage = ImageIO.read( new File("Implementierung/src/org/studien/thumbnail/schwarzwald-Urlaub.jpg") ); 
-		int percent = 50;
-		BufferedImage thumbnailed = (BufferedImage) rawImage.getScaledInstance(
-		  (rawImage.getWidth() * percent) / 100, 
-		  (rawImage.getHeight() * percent) / 100, 
-		  Image.SCALE_SMOOTH ); 
-		setImage(thumbnailed);
-
+		String tPath = "Implementierung/src/org/studien/thumbnail/";
+		BufferedImage rawImage = ImageIO.read( new File(tPath + "schwarzwald-Urlaub.jpg") ); 
+		// save the thumbnail
+		ImageIO.write(getThumbOf(rawImage, 200, Image.SCALE_DEFAULT), "jpg", new File(tPath + "schwarzwald-Urlaub_thumb_default.jpg"));
+		ImageIO.write(getThumbOf(rawImage, 200, Image.SCALE_FAST), "jpg", new File(tPath + "schwarzwald-Urlaub_thumb_fast.jpg"));
+		ImageIO.write(getThumbOf(rawImage, 200, Image.SCALE_SMOOTH), "jpg", new File(tPath + "schwarzwald-Urlaub_thumb_smooth.jpg"));
 	}
-	 public static void setImage( BufferedImage imageInput ) 
-	  { 
-	    image = imageInput; 
-	    setPreferredSize( new Dimension(image.getWidth(), image.getHeight()) ); 
-	    repaint(); 
-	    invalidate(); 
-	  } 
-	
-	
-	protected void paintComponent( Graphics g ) 
-	  { 
-	    if ( image != null ) 
-	      g.drawImage( image, 0, 0, this ); 
-	  } 
+	public static BufferedImage getThumbOf(BufferedImage bImage, int maxWidthOrHight, int hints){
+		int width = bImage.getWidth();
+		int height = bImage.getHeight();
+		
+		if (width>=height){
+			width = maxWidthOrHight;
+			height = -1; // negative Höhe bedeutet, dass diese Höhe dem Seitenverhältnis entsprechend an die neue Breite angepasst wird.
+		}else{
+			width = -1; // negative breite: Breite wird dem Seitenverhältnis entsprechend an die neue Höhe angepasst.
+			height = maxWidthOrHight;
+		}
+		
+		Image thumbnailed = bImage.getScaledInstance(width, height, hints);
+		BufferedImage bThumb = new BufferedImage(thumbnailed.getWidth(null),thumbnailed.getHeight(null), bImage.getType());
+		bThumb.createGraphics().drawImage(thumbnailed, 0, 0, null);
+		return bThumb;
+	}
 }
