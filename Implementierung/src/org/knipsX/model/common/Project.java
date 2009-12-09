3b1243df5a6d0431a0098e3bf4e63292f95b3cdc
BenniX47 <benni-borstel@online.de>
2009-12-09 23:15:47 +0100

    Weiter am Projekt gearbeitet. Neue Funktionalitaet. An Projektklasse gearbeitet. Klasse Report eingefuehrt.
    Signed-off-by: BenniX47 <benni-borstel@online.de>

--- Implementierung/src/org/knipsX/model/common/Project.java
+++ Implementierung/src/org/knipsX/model/common/Project.java
@@ -1,13 +1,50 @@
 package org.knipsX.model.common;
 
 import java.util.GregorianCalendar;
+import java.util.LinkedList;
+import java.util.List;
+import org.knipsX.model.picturemanagement.PictureContainer;
+import org.knipsX.model.picturemanagement.PictureSet;
+import org.knipsX.model.reportmanagement.Report;
 
 public class Project extends ProjectEntry {
 	
+	private List<PictureContainer> pictureSets;	
+	private List<Report> reports;	
 	
-
 	public Project(int id, String name, GregorianCalendar creationDate, String path) {
 		super(id, name, creationDate, path);
+		readFromProjectFile(path);
+	}
+	
+	public List<PictureContainer> getPictureSets() {
+		return pictureSets;
 	}
 
+	public void setPictureSets(List<PictureContainer> pictureSets) {
+		this.pictureSets = pictureSets;
+	}
+
+	public List<Report> getReports() {
+		return reports;
+	}
+
+	public void setReports(List<Report> reports) {
+		this.reports = reports;
+	}
+
+	public void readFromProjectFile(String path){
+		List<PictureContainer> dummyPictureSetList = new LinkedList<PictureContainer>();
+		PictureContainer dummyPictureSet = new PictureSet();
+		dummyPictureSetList.add(dummyPictureSet);
+		setPictureSets(dummyPictureSetList);
+		List<Report> dummyReportsList = new LinkedList<Report>();
+		Report dummyReport = new Report();
+		dummyReportsList.add(dummyReport);
+		setReports(dummyReportsList);		
+	}
+	
+	public void writeToProjectFile() {
+		
+	}
 }
