e552caba2a1e5627295252ad8a20f830fd168b5d
Kai Bouche <kai@bouche.at>
2009-12-10 23:28:35 +0100

    Formatierung & Dokumentation.
    Signed-off-by: Kai Bouche <kai@bouche.at>

--- Implementierung/src/org/knipsX/Programm.java
+++ Implementierung/src/org/knipsX/Programm.java
@@ -4,48 +4,60 @@ import java.io.File;
 import java.util.GregorianCalendar;
 import java.util.LinkedList;
 import java.util.List;
+
 import org.knipsX.model.common.ProjectEntry;
 import org.knipsX.model.projectmanagement.ProjectListModel;
-import org.knipsX.view.JAbstractView;
 import org.knipsX.view.projectmanagement.JProjectAdministration;
 
 public class Programm {
 
-	// Hier muss noch ueberlegt werden wie der Path gesetzt wird
-	private static final String projectFilePath = System.getProperty("user.home")+File.separator+".knipsX";
+    /* Der Pfas zum Projektverzeichnis */
+    private static final String projectFilePath = System.getProperty("user.home") + File.separator + ".knipsX";
 
-	/**************************************************************************
-	 * main() ist die Startmethode unseres Programms. Sie erstellt das Model, in
-	 * welchem die Projekte aus der Projektkonfigurationsdatei gespeichert sind.
-	 * Zusaetzlich wird von ihr das erste Fenster bzw. die erste View erstellt.
-	 * 
-	 * @param args
-	 *************************************************************************/
+    /**
+     * main() ist die Startmethode unseres Programms. Sie erstellt das Model, in
+     * welchem die Projekte aus der Projektkonfigurationsdatei gespeichert sind.
+     * Zusaetzlich wird von ihr das erste Fenster bzw. die erste View erstellt.
+     * 
+     * @param args
+     *            Parameter.
+     */
+    public static void main(final String[] args) {
+	final ProjectListModel projectListModel = new ProjectListModel(Programm.scanProjectListFile());
+	new JProjectAdministration(projectListModel);
+    }
 
-	public static void main(String[] args) {
-		ProjectListModel projectListModel = new ProjectListModel(scanProjectListFile());
-		JAbstractView firstWindow = new JProjectAdministration(projectListModel);
-	}
+    /**
+     * Hier wird die Projektdatei mit der Liste der Projekte ausgelesen und eine
+     * Arraylist daraus erzeugt.
+     * 
+     * TODO: Achtung!!! Der derzeitige Inhalt ist nur dummyhaft eingesetzt. Es muss auch ein Path festgelegt werden wo
+     * man diese Datei findet. Siehe hierzu Attribute "projectFilePath".
+     * 
+     * @return gibt eine Liste mit Projekten zurück.
+     */
+    public static List<ProjectEntry> scanProjectListFile() {
 
-	/**************************************************************************
-	 * Hier wird die Projektdatei mit der Liste der Projekte ausgelesen und eine
-	 * Arraylist daraus erzeugt. TODO: Achtung!!! Der derzeitige Inhalt ist nur
-	 * dummyhaft eingesetzt. Es muss auch ein Path festgelegt werden wo man
-	 * diese Datei findet. Siehe hierzu Attribute "projectFilePath".
-	 *************************************************************************/
-	public static List<ProjectEntry> scanProjectListFile() {
-		List<ProjectEntry> projectList = new LinkedList<ProjectEntry>();
-		projectList.add(new ProjectEntry(1, "Schwarzwald", new GregorianCalendar(2009, 11, 12, 13, 42, 43), ""));
-		projectList.add(new ProjectEntry(2, "Der Ehhhhhhhmer", new GregorianCalendar(2009, 11, 12, 12, 42, 43), ""));
-		return projectList;
-	}
-	
-	/**************************************************************************
-	 * Hier wird die Projektdatei neu geschrieben. TODO: Methode noch leer.
-	 * Es muss auch ein Path festgelegt werden wo man
-	 * diese Datei findet. Siehe hierzu Attribute "projectFilePath".
-	 * @param list : Liste der aktuellen Projekte
-	 *************************************************************************/
-	public static void writeProjectListFile(LinkedList<ProjectEntry> list) {		
-	}
+	/* Die Liste */
+	final List<ProjectEntry> projectList = new LinkedList<ProjectEntry>();
+
+	/* Füge Projekte hinzu */
+	projectList.add(new ProjectEntry(1, "Schwarzwald", new GregorianCalendar(2009, 11, 12, 13, 42, 43), ""));
+	projectList.add(new ProjectEntry(2, "Der Ehhhhhhhmer", new GregorianCalendar(2009, 11, 12, 12, 42, 43), ""));
+
+	/* Gib Liste zurück */
+	return projectList;
+    }
+
+    /**
+     * Hier wird die Projektdatei neu geschrieben.
+     * 
+     * TODO: Methode noch leer. Es muss auch ein Path festgelegt werden wo man diese Datei findet. Siehe hierzu
+     * Attribute "projectFilePath".
+     * 
+     * @param list
+     *            Liste der aktuellen Projekte
+     */
+    public static void writeProjectListFile(final List<ProjectEntry> list) {
+    }
 }
