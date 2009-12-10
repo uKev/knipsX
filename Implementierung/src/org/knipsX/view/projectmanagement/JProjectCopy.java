9c1e0d7f22f63a331b7c3bae9c88f38b25e0d18c
Kai Bouche <kai@bouche.at>
2009-12-11 00:00:33 +0100

    Kommentieren der Klassen.
    Signed-off-by: Kai Bouche <kai@bouche.at>

--- Implementierung/src/org/knipsX/view/projectmanagement/JProjectCopy.java
+++ Implementierung/src/org/knipsX/view/projectmanagement/JProjectCopy.java
@@ -1,7 +1,9 @@
 package org.knipsX.view.projectmanagement;
 
 import java.util.Observable;
+
 import javax.swing.JButton;
+import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
@@ -13,104 +15,123 @@ import org.knipsX.model.projectmanagement.ProjectListModel;
 import org.knipsX.view.JAbstractView;
 
 public class JProjectCopy extends JAbstractView {
-	
-	private static final long serialVersionUID = 1L;
-	
-	private JPanel jContentPane = null;
-	
-	private JTextField jTextFieldProjectName;
-	
-	private JButton jButtonConfirm;
-	private JButton jButtonCancel;
-	
-	private ProjectEntry toCopy;
-	
-	public JProjectCopy(AbstractModel model, ProjectEntry toCopy) {
-		
-		/* Rufe Konstruktor der Elternklasse auf */
-		super(model);
-		
-		/* Der Projekteintrag, der kopiert werden soll */
-		this.toCopy = toCopy;
-		
-		/* Rendere Komponenten */
-		this.initialize();
-	}
 
-	/**
-	 * This method initializes this
-	 * 
-	 * @return void
-	 */
-	private void initialize() {
-		
-		/* Fenstertitel setzen */
-		this.setTitle("Projekt kopieren");
-		
-		/* */
-		this.setLocationRelativeTo(null);
-		
-		/* Inhaltspanel setzen */
-		this.setContentPane(getJContentPane());
-		
-		/* Optimale Größe der Komponenten einstellen */
-		this.pack();
-		
-		/* Sichtbar machen */
-		this.setVisible(true);		
-	}
-	
-	private JPanel getJContentPane() {
-		if (this.jContentPane == null) {
-			this.jContentPane = new JPanel();
-			
-	        jContentPane.add(this.getJTextFieldProjectName());
-	        jContentPane.add(this.getJButtonConfirm());
-	        jContentPane.add(this.jButtonCancel());
-		}
-		return this.jContentPane;
-	}
-	
-	private JTextField getJTextFieldProjectName() {
-		if (this.jTextFieldProjectName == null) {
-			this.jTextFieldProjectName = new JTextField(20);
-		}
-		return this.jTextFieldProjectName;
-	}
-	
-	private JButton getJButtonConfirm() {
-		if (this.jButtonConfirm == null) {
-			this.jButtonConfirm = new JButton("Ok");
-			
-			this.jButtonConfirm.addActionListener(new CopyOk(this.model, this));
-		}
-		return this.jButtonConfirm;
-	}
-	
-	private JButton jButtonCancel() {
-		if (this.jButtonCancel == null) {
-			this.jButtonCancel = new JButton("Abbrechen");
-			
-			this.jButtonCancel.addActionListener(new CopyCancel(this.model));
-		}
-		return this.jButtonCancel;
-	}
+    /* Für Serialisierung */
+    private static final long serialVersionUID = 7137169546350645683L;
 
-	@Override
-	public void update(Observable o, Object arg) {
-		//Methode muss das unsichtbare Panel aktualisieren wenn zb falsche eingaben da sind für einen namen.
-		ProjectListModel model = (ProjectListModel) o;
-		if (model.getModelStatus() == ProjectListModel.COPY){			
-		} else {
-			this.dispose();
-		}
-	}
+    private JPanel jContentPane = null;
+
+    private JTextField jTextFieldProjectName;
+
+    private JButton jButtonConfirm;
+    private JButton jButtonCancel;
+    
+    private ProjectEntry projectToCopy;
+
+    public JProjectCopy(final AbstractModel model, final ProjectEntry projectToCopy) {
+
+	/* Setze Modell */
+	super(model);
+
+	/* Setze zu kopierendes Projekt */
+	this.projectToCopy = projectToCopy;
 	
-	public String getProjectName() {
-		 return jTextFieldProjectName.getText();	
-	}
+	/* Zeichne Fenster */
+	this.initialize();
+    }
+
+    /**
+     * This method initializes this
+     * 
+     * @return void
+     */
+    private void initialize() {
+
+	/* Fenstertitel setzen */
+	this.setTitle("Neues Projekt");
+
+	/* Setze das Hauptpanel */
+	this.setContentPane(this.getJContentPane());
 	
-	public ProjectEntry getProjectToCopy() {
-		return this.toCopy;
+	/* Setze Standardschließaktion */
+	/* TODO Schließaktion anpassen! */
+	this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	
+	/* Ändere Größe */
+	this.pack();
+	
+	/* Setze Lokation */
+	this.setLocationRelativeTo(null);
+	
+	/* Zeige Fensert an */
+	this.setVisible(true);
+    }
+    
+    /* Initialisiert das Hauptpanel */
+    private JPanel getJContentPane() {
+	if (this.jContentPane == null) {
+	    this.jContentPane = new JPanel();
+
+	    this.jContentPane.add(this.getJTextFieldProjectName());
+	    this.jContentPane.add(this.getJButtonConfirm());
+	    this.jContentPane.add(this.jButtonCancel());
 	}
+	return this.jContentPane;
+    }
+    
+    /* Initialisiert den Ok Button */
+    private JButton getJButtonConfirm() {
+	if (this.jButtonConfirm == null) {
+	    this.jButtonConfirm = new JButton("Ok");
+
+	    this.jButtonConfirm.addActionListener(new CopyOk(this.model, this));
+	}
+	return this.jButtonConfirm;
+    }
+
+    /* Initialisiert den Abbrechen Button */
+    private JButton jButtonCancel() {
+	if (this.jButtonCancel == null) {
+	    this.jButtonCancel = new JButton("Abbrechen");
+
+	    this.jButtonCancel.addActionListener(new CopyCancel(this.model));
+	}
+	return this.jButtonCancel;
+    }
+    
+    /* Initialisiert das Textfeld */
+    private JTextField getJTextFieldProjectName() {
+	if (this.jTextFieldProjectName == null) {
+	    this.jTextFieldProjectName = new JTextField(20);
+	}
+	return this.jTextFieldProjectName;
+    }
+
+    /* Gibt den Projektnamen zurück */
+    public String getProjectName() {
+	return this.jTextFieldProjectName.getText();
+    }    
+
+    /* Gib das Projekt zurück, das kopiert werden soll */
+    public ProjectEntry getProjectToCopy() {
+	return this.projectToCopy;
+    }
+    
+    @Override
+    public void update(final Observable o, final Object arg) {
+	
+	/* Bekomme das Modell geliefert */
+	final ProjectListModel model = (ProjectListModel) o;
+	
+	/* Methode muss das unsichtbare Panel aktualisieren wenn zb falsche eingaben da sind für einen namen. */
+	if (model.getModelStatus() == ProjectListModel.COPY) {
+	    
+	    /* Zeige Fenster an */
+	    this.setVisible(true);
+	} else {
+	    
+	    /* Lösche Fenster */
+	    this.dispose();
+	}
+    }    
 }
