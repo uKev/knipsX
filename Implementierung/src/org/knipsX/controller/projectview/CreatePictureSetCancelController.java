94889eb39a84714af11a9346b11f0695467926d3
BenniX47 <benni-borstel@online.de>
2009-12-11 18:04:34 +0100

    Habe nochmal neu controller hinzugefügt und noch ne neue View zum Pictureset erstellen
    Signed-off-by: BenniX47 <benni-borstel@online.de>

---
+++ Implementierung/src/org/knipsX/controller/projectview/CreatePictureSetCancelController.java
@@ -0,0 +1,18 @@
+package org.knipsX.controller.projectview;
+
+import java.awt.event.ActionEvent;
+
+import org.knipsX.controller.AbstractController;
+import org.knipsX.model.AbstractModel;
+
+public class CreatePictureSetCancelController extends AbstractController {
+
+	public CreatePictureSetCancelController(AbstractModel model) {
+		super(model);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+
+	}
+}
---
+++ Implementierung/src/org/knipsX/controller/projectview/CreatePictureSetOkController.java
@@ -0,0 +1,18 @@
+package org.knipsX.controller.projectview;
+
+import java.awt.event.ActionEvent;
+import org.knipsX.controller.AbstractController;
+import org.knipsX.model.AbstractModel;
+import org.knipsX.view.projectview.JNewPictureSet;
+
+public class CreatePictureSetOkController extends AbstractController {
+
+	public CreatePictureSetOkController(AbstractModel model, JNewPictureSet jNewPictureSet) {
+		super(model);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+
+	}
+}
