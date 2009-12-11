c1e86f5c76deb63d06fa58e86cf108d73bb797c6
Kai Bouche <kai@bouche.at>
2009-12-11 17:50:06 +0100

    Dateisystemfunktionalität in Utility-Klasse ausgelagert.
    Signed-off-by: Kai Bouche <kai@bouche.at>

--- Implementierung/src/org/knipsX/Programm.java
+++ Implementierung/src/org/knipsX/Programm.java
@@ -3,27 +3,15 @@
  */
 package org.knipsX;
 
-/* import things from the java sdk */
-import java.io.File;
-import java.util.GregorianCalendar;
-import java.util.LinkedList;
-import java.util.List;
-
-/* import things from our program */
-import org.knipsX.model.common.ProjectEntry;
+/* import things from our programm */
 import org.knipsX.model.projectmanagement.ProjectListModel;
+import org.knipsX.utils.FileHandler;
 import org.knipsX.view.projectmanagement.JProjectAdministration;
 
 /**
  * This class is the entry to our program.
- * 
  */
 public class Programm {
-
-    /**
-     * Determines the path to the directory where all projects of the program are stored.
-     */
-    private static final String projectFilePath = System.getProperty("user.home") + File.separator + ".knipsX";
 
     /**
      * Starts knipsX.
@@ -36,29 +24,9 @@ public class Programm {
     public static void main(final String[] args) {
 
 	/* create a model for the ProjectAdministration */
-	final ProjectListModel projectListModel = new ProjectListModel(Programm.scanProjectListFile());
+	final ProjectListModel projectListModel = new ProjectListModel(FileHandler.scanProjectDirectory());
 
 	/* creates a new JProjectAdministration window, which is connected to a model */
 	new JProjectAdministration(projectListModel);
-    }
-
-    /**
-     * Scans a the directory where all projects of the program are stored an creates for each project configuration file
-     * a ProjectEntry.
-     * 
-     * @return a list of ProjectEntry objects.
-     * @see org.knipsX.model.common.ProjectEntry
-     */
-    private static List<ProjectEntry> scanProjectListFile() {
-
-	/* the list */
-	final List<ProjectEntry> projectList = new LinkedList<ProjectEntry>();
-
-	/* add some dummy projects */
-	projectList.add(new ProjectEntry(1, "Schwarzwald", new GregorianCalendar(2009, 11, 12, 7, 9, 3), ""));
-	projectList.add(new ProjectEntry(2, "Der Ehhhhhhhmer", new GregorianCalendar(2009, 11, 12, 12, 42, 43), ""));
-
-	/* returns the list */
-	return projectList;
     }
 }
