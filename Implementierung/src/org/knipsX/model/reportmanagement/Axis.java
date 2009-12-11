3f0bb982b3cbac76e39b1ae7a4664e254e69aee0
Kevin Zuber <ukev@knet.eu>
2009-12-11 17:14:25 +0100

    mode.reportmanagement: Axis improved

--- Implementierung/src/org/knipsX/model/reportmanagement/Axis.java
+++ Implementierung/src/org/knipsX/model/reportmanagement/Axis.java
@@ -2,5 +2,26 @@ package org.knipsX.model.reportmanagement;
 
 
 public class Axis{
-	protected String description;
+
+	private String description;
+	private ExifParameter parameter;
+	
+	public Axis(String description, ExifParameter parameter) {
+		super();
+		this.description = description;
+		this.parameter = parameter;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public ExifParameter getParameter() {
+		return parameter;
+	}
+	public void setParameter(ExifParameter parameter) {
+		this.parameter = parameter;
+	}
 }
