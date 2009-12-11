5e9e9edea3519b10d471730092a8d1cb67558388
BenniX47 <benni-borstel@online.de>
2009-12-12 00:53:35 +0100

    merge
    Signed-off-by: BenniX47 <benni-borstel@online.de>

--- Implementierung/src/org/knipsX/controller/projectview/EditProjectDescriptionController.java
+++ Implementierung/src/org/knipsX/controller/projectview/EditProjectDescriptionController.java
@@ -3,6 +3,7 @@ package org.knipsX.controller.projectview;
 import java.awt.event.ActionEvent;
 import org.knipsX.controller.AbstractController;
 import org.knipsX.model.AbstractModel;
+import org.knipsX.model.projectview.ProjectViewModel;
 import org.knipsX.view.JAbstractView;
 import org.knipsX.view.projectview.JProjectView;
 
