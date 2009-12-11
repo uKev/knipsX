532a6c859d7b83105c805cd4f873c0796e285393
Kevin Zuber <ukev@knet.eu>
2009-12-11 17:40:30 +0100

    mode.reportmanagement: category improved

--- Implementierung/src/org/knipsX/model/reportmanagement/Category.java
+++ Implementierung/src/org/knipsX/model/reportmanagement/Category.java
@@ -1,7 +1,20 @@
 package org.knipsX.model.reportmanagement;
 
+/**
+ * 
+ * @author Kevin Zuber
+ * Räpresentiert eine statistische Klasse für Histogramme.
+ *
+ */
 public class Category {
 	private Bar bars[];
+
+	// X Achse
+	private int positionX;
+
+	
+	// Z Achse, Immer 0, wenn nur für ein 2D Diagramm benutzt wird
+	private int positionZ;
 
 	public Category(Bar[] bars) {
 		super();
@@ -14,6 +27,22 @@ public class Category {
 
 	public void setBars(Bar[] bars) {
 		this.bars = bars;
+	}
+
+	public int getPositionX() {
+		return positionX;
+	}
+
+	public void setPositionX(int positionX) {
+		this.positionX = positionX;
+	}
+
+	public int getPositionZ() {
+		return positionZ;
+	}
+
+	public void setPositionZ(int positionZ) {
+		this.positionZ = positionZ;
 	}
 	
 }
