fbea0bc6f47eef1fafb85baf76cc346346fd6e8e
Kevin Zuber <ukev@knet.eu>
2009-12-11 18:51:31 +0100

    mode.reportmanagement: Cluser3dModel made yAxis private

--- Implementierung/src/org/knipsX/model/reportmanagement/Cluster3DModel.java
+++ Implementierung/src/org/knipsX/model/reportmanagement/Cluster3DModel.java
@@ -2,5 +2,18 @@ package org.knipsX.model.reportmanagement;
 
 
 public class Cluster3DModel extends AbstractDoubleAxesModel{
-	protected Axis yAxis;
+	private Axis yAxis;
+
+	public Cluster3DModel(Axis yAxis) {
+		super();
+		this.yAxis = yAxis;
+	}
+
+	public Axis getyAxis() {
+		return yAxis;
+	}
+
+	public void setyAxis(Axis yAxis) {
+		this.yAxis = yAxis;
+	}
 }
