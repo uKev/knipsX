5f49f2cb63a1f67e21ba4196b416eb689926a3dd
Kevin Zuber <ukev@knet.eu>
2009-12-11 17:08:04 +0100

    Revert "mode.reportmanagement: Categorie for Histograms added"
    This reverts commit fe1dfbf1381b2158294a487af99b261fd9dbbea9.

--- Implementierung/src/org/knipsX/model/reportmanagement/Axis.java
+++ Implementierung/src/org/knipsX/model/reportmanagement/Axis.java
@@ -2,26 +2,5 @@ package org.knipsX.model.reportmanagement;
 
 
 public class Axis{
-
-	private String description;
-	private ExifParameter parameter;
-	
-	public Axis(String description, ExifParameter parameter) {
-		super();
-		this.description = description;
-		this.parameter = parameter;
-	}
-
-	public String getDescription() {
-		return description;
-	}
-	public void setDescription(String description) {
-		this.description = description;
-	}
-	public ExifParameter getParameter() {
-		return parameter;
-	}
-	public void setParameter(ExifParameter parameter) {
-		this.parameter = parameter;
-	}
+	protected String description;
 }
