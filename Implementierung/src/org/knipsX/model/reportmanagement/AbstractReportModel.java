3e735541ad1f91fad03621d336a887b15530df25
David Kaufman <david@david-desktop.(none)>
2009-12-11 09:36:41 +0100

    AbstractReportModel erbt von AbstractModel

--- Implementierung/src/org/knipsX/model/reportmanagement/AbstractReportModel.java
+++ Implementierung/src/org/knipsX/model/reportmanagement/AbstractReportModel.java
@@ -1,8 +1,10 @@
 package org.knipsX.model.reportmanagement;
 
 import java.util.ArrayList;
+
+import org.knipsX.model.AbstractModel;
 import org.knipsX.model.picturemanagement.PictureContainer;
 
-public abstract class AbstractReportModel{
+public abstract class AbstractReportModel extends AbstractModel {
 	protected ArrayList<PictureContainer> pictureContainer;
 }
