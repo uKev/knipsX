803bc9e7c4816e49db58c168a329a7f05b3bcf9e
BenniX47 <benni-borstel@online.de>
2009-12-11 17:37:03 +0100

    Controller erstellt nur fuer das Hauptfenster
    Signed-off-by: BenniX47 <benni-borstel@online.de>

---
+++ Implementierung/src/org/knipsX/controller/projectview/CreatePictureSetController.java
@@ -0,0 +1,18 @@
+package org.knipsX.controller.projectview;
+
+import java.awt.event.ActionEvent;
+
+import org.knipsX.controller.AbstractController;
+import org.knipsX.model.AbstractModel;
+
+public class CreatePictureSetController extends AbstractController {
+
+	public CreatePictureSetController(AbstractModel model) {
+		super(model);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+
+	}
+}
--- Implementierung/src/org/knipsX/controller/projectview/SwitchProjectController.java
+++ Implementierung/src/org/knipsX/controller/projectview/SwitchProjectController.java
@@ -1,7 +1,6 @@
 package org.knipsX.controller.projectview;
 
 import java.awt.event.ActionEvent;
-
 import org.knipsX.Programm;
 import org.knipsX.controller.AbstractController;
 import org.knipsX.model.AbstractModel;
@@ -11,20 +10,17 @@ import org.knipsX.view.JAbstractView;
 import org.knipsX.view.projectmanagement.JProjectAdministration;
 
 public class SwitchProjectController extends AbstractController {
-	
-	private ProjectViewModel model;
-			
+
 	public SwitchProjectController(AbstractModel model) {
-		this.model = (ProjectViewModel) model;
+		super(model);
 	}
 
 	@Override
-	public void actionPerformed(ActionEvent e) {		
-		/*  Aktuelle View unsichtbar machen */
+	public void actionPerformed(ActionEvent e) {
 		model.setModelStatus(ProjectViewModel.SWITCHPROJECT);
 		model.updateViews();
-		ProjectListModel projectListModel = new ProjectListModel(Programm.scanProjectListFile());
-		JAbstractView firstWindow = new JProjectAdministration(projectListModel);		
+		ProjectListModel projectListModel = new ProjectListModel(Programm
+				.scanProjectListFile());
+		JAbstractView firstWindow = new JProjectAdministration(projectListModel);
 	}
 }
-
