7d94acdd442824cd0e30c63172a5e18d674871f5
BenniX47 <benni-borstel@online.de>
2009-12-11 23:10:11 +0100

    Wieder mal weiter Controller erstellt und gleich eingebunden
    Signed-off-by: BenniX47 <benni-borstel@online.de>

---
+++ Implementierung/src/org/knipsX/controller/projectview/SwitchProjectSaveYesController.java
@@ -0,0 +1,25 @@
+package org.knipsX.controller.projectview;
+
+import java.awt.event.ActionEvent;
+import org.knipsX.controller.AbstractController;
+import org.knipsX.model.AbstractModel;
+import org.knipsX.model.projectmanagement.ProjectListModel;
+import org.knipsX.model.projectview.ProjectViewModel;
+import org.knipsX.utils.FileHandler;
+import org.knipsX.view.projectmanagement.JProjectAdministration;
+
+public class SwitchProjectSaveYesController extends AbstractController {
+	
+	public SwitchProjectSaveYesController(AbstractModel model) {
+		super(model);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		this.model.setModelStatus(ProjectViewModel.SWITCHPROJECT);
+		//TODO saveToProjectFile(model); in filehandel;
+		model.updateViews();
+		final ProjectListModel projectListModel = new ProjectListModel(FileHandler.scanProjectDirectory());
+		new JProjectAdministration(projectListModel);
+	}
+}
