bcba93d30170130e299cc1e68b8b787c62d273aa
David Kaufman <david.kaufman@gmx.de>
2009-12-13 02:42:32 +0100

    Minimale Änderung an der Klasse

---
+++ Implementierung/src/org/knipsX/view/diagrams/SelectableCluster3DSphere.java
@@ -0,0 +1,21 @@
+package org.knipsX.view.diagrams;
+
+import javax.media.j3d.Shape3D;
+import org.knipsX.model.picturemanagement.Picture;
+import com.sun.j3d.utils.geometry.Sphere;
+
+public class SelectableCluster3DSphere extends Shape3D {
+	
+	private Picture picture;
+	
+	public SelectableCluster3DSphere(Picture picture) {	
+		this.picture = picture;
+        Sphere mySphere = new Sphere(0.075f*2,Sphere.GENERATE_NORMALS,JAbstract3DView.GEODETAIL);
+        addGeometry(mySphere.getShape().getGeometry());
+	}
+
+	public Picture getPicture() {
+		return picture;
+	}
+
+}
