32f96f23a179326a7faab3d88203420471116425
David Kaufman <david.kaufman@gmx.de>
2009-12-14 01:59:01 +0100

    AbstractTrippleAxesModel hinzugefügt

---
+++ Implementierung/src/org/knipsX/model/reportmanagement/AbstractTrippleAxesModel.java
@@ -0,0 +1,34 @@
+package org.knipsX.model.reportmanagement;
+
+import java.util.ArrayList;
+import org.knipsX.model.picturemanagement.PictureContainer;
+
+
+/**
+ * AbstractTrippleAxesModel is the superclass of all diagram models which need at least three
+ * axes with exif parameters and exif description.
+ * 
+ * @author David Kaufman
+ *
+ */
+public abstract class AbstractTrippleAxesModel extends AbstractDoubleAxesModel {
+
+	// needs to be protected because it's used in subclass
+	protected Axis yAxis;	
+
+
+	public AbstractTrippleAxesModel(
+			ArrayList<PictureContainer> pictureContainer, Axis xAxis, Axis zAxis, Axis yAxis) {
+		super(pictureContainer, xAxis, zAxis);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+	public Axis getyAxis() {
+		return yAxis;
+	}
+
+	public void setyAxis(Axis yAxis) {
+		this.yAxis = yAxis;
+	}
+}
