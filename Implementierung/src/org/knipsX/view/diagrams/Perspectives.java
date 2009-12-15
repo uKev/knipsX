b86db05f60aba14af1872b6a0b9ab0d79dc882cf
David Kaufman <david.kaufman@gmx.de>
2009-12-16 00:21:45 +0100

    Ein wenig an den Diagrammen gearbeitet

---
+++ Implementierung/src/org/knipsX/view/diagrams/Perspectives.java
@@ -0,0 +1,17 @@
+package org.knipsX.view.diagrams;
+
+/**
+ * This enumeration lists all predefined camera perspectives.
+ * 
+ * @author David Kaufman
+ *
+ */
+public enum Perspectives {
+	
+	XYPLANE,
+	XZPLANE,
+	YZPLANE,
+	PERSPECTIVE;
+}
+
+
