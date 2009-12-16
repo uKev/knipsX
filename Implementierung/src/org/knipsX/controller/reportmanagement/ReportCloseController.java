9c2baebcf262c069d1b97ea64983e057f7ec8886
David Kaufman <david@david-desktop.(none)>
2009-12-16 17:13:55 +0100

    Wieder Namen geändert

---
+++ Implementierung/src/org/knipsX/controller/reportmanagement/ReportCloseController.java
@@ -0,0 +1,27 @@
+package org.knipsX.controller.reportmanagement;
+
+import java.awt.event.ActionEvent;
+
+import org.knipsX.controller.AbstractController;
+
+/**
+ * This controller manages the closure of the report configuration view.
+ * 
+ * @author David Kaufman
+ *
+ * @param <M>
+ * @param <V>
+ */
+public class ReportCloseController<M, V> extends AbstractController<M, V> {
+
+    public ReportCloseController(V view) {
+	super(view);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+	// TODO Auto-generated method stub
+
+    }
+
+}
