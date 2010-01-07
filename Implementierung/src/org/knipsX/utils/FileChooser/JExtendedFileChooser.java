5df6450195fdff6909c0e4971ee69d0beb7b32f5
David Kaufman <david.kaufman@gmx.de>
2010-01-07 01:56:55 +0100

    FileChooser mit ImageDisplayer hinzugefügt

---
+++ Implementierung/src/org/knipsX/utils/FileChooser/JExtendedFileChooser.java
@@ -0,0 +1,90 @@
+package org.knipsX.utils.FileChooser;
+
+import java.awt.image.BufferedImage;
+import java.io.*;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+
+public class JExtendedFileChooser {
+	
+	private static JFileChooser filechooser;
+	private static String lastSaveDir = "";
+	private static String lastOpenDir = "";
+	
+
+    public static void selectDirectoriesAndImages() {  
+    	initialize();
+    	
+    	filechooser.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+    	filechooser.setMultiSelectionEnabled(true);    	
+    	filechooser.setCurrentDirectory(new File(lastOpenDir));
+    	
+	    //Add a custom file filter and disable the default
+	    //(Accept All) file filter.
+    	filechooser.addChoosableFileFilter(new ImageFilter());
+    	filechooser.setAcceptAllFileFilterUsed(false);
+            
+       
+	    //Add the preview pane.
+    	filechooser.setAccessory(new ImagePreview(filechooser));
+        
+        //Show it.
+        int returnVal = filechooser.showDialog(filechooser,"Attach");
+        
+        //Process the results.
+        if (returnVal == JFileChooser.APPROVE_OPTION) {
+            File file = filechooser.getSelectedFile(); 
+            lastOpenDir = file.getAbsolutePath();
+        }
+        
+        //Reset the file chooser for the next time it's shown.
+        filechooser.setSelectedFile(null);        
+
+    }
+    
+    private static void initialize() {
+    	if(filechooser == null) {
+    		filechooser = new JFileChooser();
+    	} 
+    	
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (Exception exc) {
+			System.err.println("Error loading  Look and Feel: " + exc);
+		}
+    }
+    
+    public static void saveBufferedImage(BufferedImage imageToBeSaved) {
+
+    	initialize();
+    	
+    	filechooser.setFileSelectionMode(JFileChooser.SAVE_DIALOG);
+    	filechooser.setMultiSelectionEnabled(false);  
+    	filechooser.setCurrentDirectory(new File(lastSaveDir));
+    	
+        //Handle save button action.
+        int returnVal = filechooser.showSaveDialog(null);
+        
+        if (returnVal == JFileChooser.APPROVE_OPTION) {
+            File file = filechooser.getSelectedFile();     
+            
+            lastSaveDir = file.getAbsolutePath();
+            
+    		File outputfile = new File(file.getAbsolutePath().replaceAll(".png", "").replaceAll(".png", "")+".png");
+    		
+    		
+    		// Uncomment for debugging purposes 
+    		//new ImageDisplayer(imageToBeSaved);
+    		
+    		try {
+    			ImageIO.write(imageToBeSaved, "png", outputfile);
+    		} catch (IOException e1) {
+    			e1.printStackTrace();
+    		}
+            
+        }
+    }
+    
+
+}
