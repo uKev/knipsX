48e1f86cac45935ccc891fd3e06891445e3f937a
David Kaufman <david.kaufman@gmx.de>
2010-01-21 00:37:06 +0100

    ReportHelper komplett

--- Implementierung/src/org/knipsX/view/reportmanagement/ReportHelper.java
+++ Implementierung/src/org/knipsX/view/reportmanagement/ReportHelper.java
@@ -29,124 +29,350 @@ import org.knipsX.view.diagrams.JTableDiagram;
  */
 public enum ReportHelper {
 
+    /** The report which identifies a boxplot report **/
     Boxplot {
+        /**
+         * {@inheritDoc}
+         */
+        @Override
         public AbstractReportCompilation createReportCompilation() {
             ReportHelper.currentReport = ReportHelper.Boxplot;
             return new BoxplotConfig();
         }
 
-        public AbstractReportModel createReportModel() {            
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public AbstractReportModel createReportModel() {
             return new BoxplotModel();
         }
 
-        public JAbstractDiagram<AbstractReportModel> displayDiagram(AbstractReportModel model, int reportID) {
-            return new JBoxplot((BoxplotModel) model, reportID);
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public JAbstractDiagram<AbstractReportModel> displayDiagram(final AbstractReportModel model, final int reportID) {
+            return new JBoxplot<AbstractReportModel>(model, reportID);
         }
 
+        /**
+         * {@inheritDoc}
+         */
+        @Override
         public Component getDiagramView() {
-            return new JBoxplot(null, -1).getDiagram();
+            return new JBoxplot<AbstractReportModel>(null, -1).getDiagram();
         };
 
+        /**
+         * {@inheritDoc}
+         */
+        @Override
         public int getNumberOfAxes() {
             return 1;
         }
     },
 
+    /** The report which identifies a 2D histogram report **/
     Histogram2D {
+        /**
+         * {@inheritDoc}
+         */
+        @Override
         public AbstractReportCompilation createReportCompilation() {
             ReportHelper.currentReport = ReportHelper.Histogram2D;
             return new Histogram2DConfig();
         }
 
+        /**
+         * {@inheritDoc}
+         */
+        @Override
         public AbstractReportModel createReportModel() {
             return new Histogram2DModel();
         }
 
-        public JAbstractDiagram<AbstractReportModel> displayDiagram(AbstractReportModel model, int reportID) {
-            return new JHistogram2D((Histogram2DModel) model, reportID);
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public JAbstractDiagram<AbstractReportModel> displayDiagram(final AbstractReportModel model, final int reportID) {
+            return new JHistogram2D<AbstractReportModel>(model, reportID);
         }
 
+        /**
+         * {@inheritDoc}
+         */
+        @Override
         public Component getDiagramView() {
-            return new JHistogram2D(null, -1).getDiagram();
+            return new JHistogram2D<AbstractReportModel>(null, -1).getDiagram();
         };
 
+        /**
+         * {@inheritDoc}
+         */
+        @Override
         public int getNumberOfAxes() {
             return 1;
         }
     },
 
+    /** The report which identifies a 3D histogram report **/
     Histogram3D {
+        /**
+         * {@inheritDoc}
+         */
+        @Override
         public AbstractReportCompilation createReportCompilation() {
             ReportHelper.currentReport = ReportHelper.Histogram3D;
             return new Histogram3DConfig();
         }
 
+        /**
+         * {@inheritDoc}
+         */
+        @Override
         public AbstractReportModel createReportModel() {
             return new Histogram3DModel();
         }
 
-        public JAbstractDiagram<AbstractReportModel> displayDiagram(AbstractReportModel model, int reportID) {
-            return new JHistogram3D((Histogram3DModel) model, reportID);
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public JAbstractDiagram<AbstractReportModel> displayDiagram(final AbstractReportModel model, final int reportID) {
+            return new JHistogram3D<AbstractReportModel>(model, reportID);
         }
 
+        /**
+         * {@inheritDoc}
+         */
+        @Override
         public Component getDiagramView() {
-            return new JHistogram3D(null, -1).getDiagram();
+            return new JHistogram3D<AbstractReportModel>(null, -1).getDiagram();
         };
 
+        /**
+         * {@inheritDoc}
+         */
+        @Override
         public int getNumberOfAxes() {
             return 2;
         }
 
     },
+
+    /** The report which identifies a 3D cluster report **/
     Cluster3D {
+        /**
+         * {@inheritDoc}
+         */
+        @Override
         public AbstractReportCompilation createReportCompilation() {
             ReportHelper.currentReport = ReportHelper.Cluster3D;
             return new Cluster3DConfig();
         }
 
+        /**
+         * {@inheritDoc}
+         */
+        @Override
         public AbstractReportModel createReportModel() {
             return new Cluster3DModel();
         }
 
-        public JAbstractDiagram<AbstractReportModel> displayDiagram(AbstractReportModel model, int reportID) {
-            return new JCluster3D((Cluster3DModel) model, reportID);
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public JAbstractDiagram<AbstractReportModel> displayDiagram(final AbstractReportModel model, final int reportID) {
+            return new JCluster3D<AbstractReportModel>(model, reportID);
         }
 
+        /**
+         * {@inheritDoc}
+         */
+        @Override
         public Component getDiagramView() {
-            return new JCluster3D(null, -1).getDiagram();
+            return new JCluster3D<AbstractReportModel>(null, -1).getDiagram();
         };
 
+        /**
+         * {@inheritDoc}
+         */
+        @Override
         public int getNumberOfAxes() {
             return 3;
         }
 
     },
 
+    /** The report which identifies a table report **/
     Table {
 
+        /**
+         * {@inheritDoc}
+         */
+        @Override
         public AbstractReportCompilation createReportCompilation() {
             ReportHelper.currentReport = ReportHelper.Table;
             return new TableConfig();
         }
 
+        /**
+         * {@inheritDoc}
+         */
+        @Override
         public AbstractReportModel createReportModel() {
             return new TableModel(null);
         }
 
-        public JAbstractDiagram<AbstractReportModel> displayDiagram(AbstractReportModel model, int reportID) {
-            return new JTableDiagram((TableModel) model, reportID);
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public JAbstractDiagram<AbstractReportModel> displayDiagram(final AbstractReportModel model, final int reportID) {
+            return new JTableDiagram<AbstractReportModel>(model, reportID);
         }
 
+        /**
+         * {@inheritDoc}
+         */
+        @Override
         public Component getDiagramView() {
-            return new JTableDiagram(null, -1).getDiagram();
+            return new JTableDiagram<AbstractReportModel>(null, -1).getDiagram();
         };
 
+        /**
+         * {@inheritDoc}
+         */
+        @Override
         public int getNumberOfAxes() {
             return 0;
         }
     };
 
-    public static int reportID = -1;
+    /**
+     * The current report of the current report configuration run
+     */
+    private static ReportHelper currentReport;
+
+    /**
+     * The default report which is selected automatically when you start the Wizard
+     */
+    private static ReportHelper defaultReport = ReportHelper.Boxplot;
+
+    /**
+     * The current configuration utility of the current report configuration run
+     */
+    protected static JAbstractReportUtil<?> currentReportUtil;
+
+    /**
+     * The current model of the current report configuration run
+     * 
+     */
+    // public because controller needs access
+    private static AbstractReportModel currentModel;
+
+    // public because controller needs access
+    private static ProjectModel currentProjectModel;
+
+    /**
+     * Returns the current model
+     * 
+     * @return The current model which is associated with the current report
+     */
+    public static AbstractReportModel getCurrentModel() {
+        return ReportHelper.currentModel;
+    }
+
+    /**
+     * Returns the current model of the report
+     * 
+     * @return the current model
+     */
+    public static ProjectModel getCurrentProjectModel() {
+        return ReportHelper.currentProjectModel;
+    }
+
+    /**
+     * Returns the current report
+     * 
+     * @return the current report
+     */
+    public static ReportHelper getCurrentReport() {
+        return ReportHelper.currentReport;
+    }
+
+    /**
+     * Returns the default report which is preselected in the wizard
+     * 
+     * @return the default report
+     */
+    public static ReportHelper getDefaultReport() {
+        return ReportHelper.defaultReport;
+    }
+
+    /**
+     * Returns a reference to the project model
+     * 
+     * @return the current project model
+     */
+    public static ProjectModel getProjectModel() {
+        return ReportHelper.currentProjectModel;
+    }
+
+    /**
+     * Sets the current report model to the specified model
+     * 
+     * @param currentModel
+     *            the current report model
+     */
+    public static void setCurrentModel(final AbstractReportModel currentModel) {
+        ReportHelper.currentModel = currentModel;
+    }
+
+    /**
+     * Sets the current project model
+     * 
+     * @param currentProjectModel
+     *            the current project model
+     */
+    public static void setCurrentProjectModel(final ProjectModel currentProjectModel) {
+        ReportHelper.currentProjectModel = currentProjectModel;
+    }
+
+    /**
+     * Sets the current report to the specified report
+     * 
+     * @param currentReport
+     *            the current report
+     */
+    public static void setCurrentReport(final ReportHelper currentReport) {
+        ReportHelper.currentReport = currentReport;
+    }
+
+    /**
+     * Sets the project model to the specified model
+     * 
+     * @param projectModel
+     *            the current project model
+     */
+    public static void setProjectModel(final ProjectModel projectModel) {
+        ReportHelper.currentProjectModel = projectModel;
+    }
+
+    /**
+     * Sets the current report and updates the configuration utility
+     * Use this method if you want explicitly update the configuration utility
+     * 
+     * @param myreport
+     *            The report you want to update
+     */
+    public static void updateReport(final ReportHelper myreport) {
+        ReportHelper.currentReport = myreport;
+        ReportHelper.currentReportUtil.setReportType(ReportHelper.currentReport.createReportCompilation());
+    }
 
     /**
      * Returns the report type associated with the specified report enum
@@ -175,7 +401,9 @@ public enum ReportHelper {
     public abstract JAbstractDiagram<AbstractReportModel> displayDiagram(AbstractReportModel model, int reportID);
 
     /**
-     * Returns the Component of the Diagram without the associated buttons
+     * Returns the Component of the Diagram without the associated button
+     * 
+     * @return the component diagram
      */
     public abstract Component getDiagramView();
 
@@ -186,45 +414,5 @@ public enum ReportHelper {
      *         returns 0 if no axis is used
      */
     public abstract int getNumberOfAxes();
-
-    /**
-     * The current report of the current report configuration run
-     */
-    public static ReportHelper currentReport;
-
-    /**
-     * The default report which is selected automatically when you start the Wizard
-     */
-    public static ReportHelper defaultReport = ReportHelper.Boxplot;
-
-    /**
-     * The current configuration utility of the current report configuration run
-     */
-    protected static JAbstractReportUtil currentReportUtil;
-
-    /**
-     * The current model of the current report configuration run
-     * 
-     */
-    // public because controller needs access
-    public static AbstractReportModel currentModel;
-
-    /**
-     * The current reference of the ProjectModel. This makes it fairly easy
-     * to add new reports to the project model.
-     */
-    // public because controller needs access
-    public static ProjectModel currentProjectModel;
-
-    /**
-     * Sets the current report and updates the configuration utility
-     * Use this method if you want explicitly update the configuration utility
-     * 
-     * @param myreport
-     */
-    public static void updateReport(ReportHelper myreport) {
-        currentReport = myreport;
-        currentReportUtil.setReportType(currentReport.createReportCompilation());
-    }
 
 }
