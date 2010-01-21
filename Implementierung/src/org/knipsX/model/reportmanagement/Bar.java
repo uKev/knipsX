c72b5c635d0a21ebf5db93045b00d55aed579a0d
Kevin Zuber <ukev@knet.eu>
2010-01-21 11:04:55 +0100

    Bar implementiert. Closes #98.

--- Implementierung/src/org/knipsX/model/reportmanagement/Bar.java
+++ Implementierung/src/org/knipsX/model/reportmanagement/Bar.java
@@ -4,57 +4,61 @@ import org.knipsX.model.picturemanagement.PictureContainer;
 
 /**
  * Represents a bar which belongs to a picture category and PictureContainer.
+ * It is primarily an encapsulated data container.
  * 
  * @author Kevin Zuber
- *
+ * 
  */
 public class Bar {
-	
-	private PictureContainer pictureContainer;
-	
-	/*
-	 * represents the absolute height of the bar.
-	 */
-	private int height;
 
-	/**
-	 * Constructor for the bar with pictureContainer and height parameter
-	 * @param pictureContainer the picture container which is represented through the bar
-	 * @param height the absolute height of the bar
-	 */
-	public Bar(PictureContainer pictureContainer, int height) {
-		super();
-		this.pictureContainer = pictureContainer;
-		this.height = height;
-	}
-	
-	/**
-	 * Constructor for the bar with pictureContainer parameter
-	 * @param pictureContainer the picture container which is represented through the bar
-	 */
-	public Bar(PictureContainer pictureContainer) {
-		super();
-		this.pictureContainer = pictureContainer;
-	}
-	
-	
-	public PictureContainer getPictureContainer() {
-		return pictureContainer;
-	}
+    private final PictureContainer pictureContainer;
 
-	public void setPictureContainer(PictureContainer pictureContainer) {
-		this.pictureContainer = pictureContainer;
-	}
+    /*
+     * represents the absolute height of the bar.
+     */
+    private int height;
 
-	public int getHeight() {
-		return height;
-	}
+    /**
+     * Constructor for the bar with pictureContainer parameter
+     * 
+     * @param pictureContainer
+     *            the picture container which is represented through the bar
+     */
+    public Bar(final PictureContainer pictureContainer) {
+        super();
+        this.pictureContainer = pictureContainer;
+    }
 
-	public void setHeight(int height) {
-		this.height = height;
-	}
+    /**
+     * Constructor for the bar with pictureContainer and height parameter
+     * 
+     * @param pictureContainer
+     *            the picture container which is represented through the bar
+     * @param height
+     *            the absolute height of the bar
+     */
+    public Bar(final PictureContainer pictureContainer, final int height) {
+        super();
+        this.pictureContainer = pictureContainer;
+        this.height = height;
+    }
 
+    /**
+     * Getter for the Height of the bar.
+     * 
+     * @return height the height of the bar.
+     */
+    public int getHeight() {
+        return this.height;
+    }
 
-	
+    /**
+     * Getter for the PictureContainer which is represented trough this bar.
+     * 
+     * @return pictureContainer the pictureContainer
+     */
+    public PictureContainer getPictureContainer() {
+        return this.pictureContainer;
+    }
 
 }
