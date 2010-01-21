c07f5cb6882df3f20c5d033092df8fd7bd969722
Kevin Zuber <ukev@knet.eu>
2010-01-21 15:46:05 +0100

    Axis implementiert. Closes #87

--- Implementierung/src/org/knipsX/model/reportmanagement/Axis.java
+++ Implementierung/src/org/knipsX/model/reportmanagement/Axis.java
@@ -5,45 +5,76 @@ import org.knipsX.utils.ExifParameter;
 /**
  * Represents an axis in a diagram. It saves which exif parameter will be used for the axis
  * and how it will be named in the diagram.
+ * 
  * @author Kevin Zuber
- *
+ * 
  */
-public class Axis{
+public class Axis {
 
-	private String description;
-	private ExifParameter parameter;
-	
-	/**
-	 * Constructor with all parameters for an axis.
-	 * @param description text for the axis
-	 * @param parameter the exif-parameter which will be evaluated for the axis
-	 */
-	public Axis(String description, ExifParameter parameter) {
-		super();
-		this.description = description;
-		this.parameter = parameter;
-	}
-	
-	/**
-	 * Constructor with exif parameter
-	 * @param parameter the exif-parameter which will be evaluated for the axis
-	 */
-	public Axis(ExifParameter parameter) {
-		super();
-		this.description = parameter.toString();
-		this.parameter = parameter;
-	}
+    private String description;
+    private ExifParameter parameter;
 
-	public String getDescription() {
-		return description;
-	}
-	public void setDescription(String description) {
-		this.description = description;
-	}
-	public ExifParameter getParameter() {
-		return parameter;
-	}
-	public void setParameter(ExifParameter parameter) {
-		this.parameter = parameter;
-	}
+    /**
+     * Constructor with exif parameter
+     * 
+     * @param parameter
+     *            the exif-parameter which will be evaluated for the axis
+     */
+    public Axis(final ExifParameter parameter) {
+        super();
+        this.description = parameter.toString();
+        this.parameter = parameter;
+    }
+
+    /**
+     * Constructor with all parameters for an axis.
+     * 
+     * @param description
+     *            text for the axis
+     * @param parameter
+     *            the exif-parameter which will be evaluated for the axis
+     */
+    public Axis(final String description, final ExifParameter parameter) {
+        super();
+        this.description = description;
+        this.parameter = parameter;
+    }
+
+    /**
+     * Getter for the description of the exif paramater in this axis.
+     * 
+     * @return the description of the exif parameter
+     */
+    public String getDescription() {
+        return this.description;
+    }
+
+    /**
+     * Getter for the ExifParameter
+     * 
+     * @return ExifParameter
+     */
+    public ExifParameter getParameter() {
+        return this.parameter;
+    }
+
+    /**
+     * Changes the description of the exif parameter
+     * 
+     * @param description
+     *            description of the exif parameter
+     */
+    public void setDescription(final String description) {
+        this.description = description;
+    }
+
+    /**
+     * Changes the parameter of type ExifParameter
+     * 
+     * @param parameter
+     *            prameter of type ExifParameter
+     */
+    public void setParameter(final ExifParameter parameter) {
+        this.parameter = parameter;
+    }
 }
