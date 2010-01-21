5d4adb5e504029c772b9ce38aa62926152807530
BenniX47 <benni-borstel@online.de>
2010-01-21 21:36:08 +0100

    Ein bisschen am Filehandler gearbeitet
    Signed-off-by: BenniX47 <benni-borstel@online.de>

--- Implementierung/src/org/knipsX/utils/FileHandler.java
+++ Implementierung/src/org/knipsX/utils/FileHandler.java
@@ -21,141 +21,149 @@ import org.knipsX.model.reportmanagement.BoxplotModel;
 /**
  * Has functions that can handle with the filesystem.
  */
-public class FileHandler {
+public final class FileHandler {
 
-	/**
-	 * Determines the path to the directory where all projects of the program are stored.
-	 */
-	public static final String PROJECTS_PATH = System.getProperty("user.home") + File.separator + ".knipsX";
+    /**
+     * Determines the path to the directory where all projects of the program are stored.
+     */
+    public static final String PROJECTS_PATH = System.getProperty("user.home") + File.separator + ".knipsX";
 
-	/* utility class - no constructor allowed! */
-	private FileHandler() {
-	}
+    /* utility class - no constructor allowed! */
+    private FileHandler() {
+    }
 
-	/**
-	 * Creates a new project configuration file.
-	 * 
-	 * @param newProject
-	 *            the new project to be saved.
-	 */
-	public static void createNewProjectFile(final ProjectEntry newProject) {
-	}
+    /**
+     * Creates a new project configuration file.
+     * 
+     * @param newProject
+     *            the new project to be saved.
+     */
+    public static void createNewProjectFile(final ProjectEntry newProject) {
+    }
 
-	/**
-	 * Deletes a project configuration file and thus the project.
-	 * 
-	 * @param newProject
-	 *            the project to be deleted.
-	 */
-	public static void deleteProjectFile(final ProjectEntry newProject) {
-	}
+    /**
+     * Deletes a project configuration file and thus the project.
+     * 
+     * @param newProject
+     *            the project to be deleted.
+     */
+    public static void deleteProjectFile(final ProjectEntry newProject) {
+    }
 
-	/**
-	 * Scans a the directory where all projects of the program are stored an creates for each project configuration file
-	 * a ProjectEntry.
-	 * 
-	 * @return a list of ProjectEntry objects.
-	 */
-	public static List<ProjectEntry> scanProjectDirectory() {
+    /**
+     * Scans a the directory where all projects of the program are stored an creates for each project configuration file
+     * a ProjectEntry.
+     * 
+     * @return a list of ProjectEntry objects.
+     */
+    public static List<ProjectEntry> scanProjectDirectory() {
 
-		/* the list */
-		final List<ProjectEntry> projectList = new LinkedList<ProjectEntry>();
+        /* the list */
+        final List<ProjectEntry> projectList = new LinkedList<ProjectEntry>();
 
-		/* add some dummy projects */
-		projectList.add(new ProjectEntry(1, "Schwarzwald", "", new GregorianCalendar(2009, 11, 12, 7, 9, 3), ""));
-		projectList
-				.add(new ProjectEntry(2, "Der Ehhhhhhhmer", "", new GregorianCalendar(2009, 11, 12, 12, 42, 43), ""));
+        File knipsXDirectory = new File(PROJECTS_PATH);
 
-		/* returns the list */
-		return projectList;
-	}
+        // String[] rootDirectories = knipsXDirectory.list();
 
-	/**
-	 * Scans a project configuration file and sets the data (e.g. create picture containers).
-	 * 
-	 * @param projectId
-	 *            the Id of the project configuration file.
-	 * 
-	 * @return a model which contains all data for a project.
-	 */
-	public static ProjectModel scanProjectFile(int projectId) {
+        // for (int n = 0; n < rootDirectories.length; n++) {
+        // System.out.println(rootDirectories[n]);
+        // }
 
-		/* TODO create private methods for the whole shit! */
+        /* add some dummy projects */
+        projectList.add(new ProjectEntry(1, "Schwarzwald", "", new GregorianCalendar(2009, 11, 12, 7, 9, 3), ""));
+        projectList
+                .add(new ProjectEntry(2, "Der Ehhhhhhhmer", "", new GregorianCalendar(2009, 11, 12, 12, 42, 43), ""));
 
-		/* create a list of picture sets */
-		List<PictureSet> pictureSetList = new LinkedList<PictureSet>();
+        /* returns the list */
+        return projectList;
+    }
 
-		/* create a list of reports */
-		List<AbstractReportModel> reportList = new LinkedList<AbstractReportModel>();
+    /**
+     * Scans a project configuration file and sets the data (e.g. create picture containers).
+     * 
+     * @param projectId
+     *            the Id of the project configuration file.
+     * 
+     * @return a model which contains all data for a project.
+     */
+    public static ProjectModel scanProjectFile(int projectId) {
 
-		/* create the project */
-		ProjectEntry project = new ProjectEntry(projectId, "XML", "Desc", new GregorianCalendar(2009, 11, 12, 7, 9, 3),
-				"");
+        /* TODO create private methods for the whole shit! */
 
-		/* create the first picture set an some picture containers */
-		PictureSet dummyPictureSet = new PictureSet("Goldfische", 1);
+        /* create a list of picture sets */
+        List<PictureSet> pictureSetList = new LinkedList<PictureSet>();
 
-		/* create some dummy picture containers and add to the picture set */
-		Picture dummyPicture = new Picture();
-		dummyPicture.setName("Nemo");
-		dummyPictureSet.add(dummyPicture);
+        /* create a list of reports */
+        List<AbstractReportModel> reportList = new LinkedList<AbstractReportModel>();
 
-		Directory dummyDirectory = new Directory();
-		dummyDirectory.setName("Urlaubsbilder");
-		dummyPictureSet.add(dummyDirectory);
+        /* create the project */
+        ProjectEntry project = new ProjectEntry(projectId, "XML", "Desc", new GregorianCalendar(2009, 11, 12, 7, 9, 3),
+                "");
 
-		dummyPictureSet.add(new PictureSet("Grillfest", 10));
-		dummyPictureSet.add(new PictureSet("SuperGoldfische", 11));
+        /* create the first picture set an some picture containers */
+        PictureSet dummyPictureSet = new PictureSet("Goldfische", 1);
 
-		/* add to list */
-		pictureSetList.add(dummyPictureSet);
+        /* create some dummy picture containers and add to the picture set */
+        Picture dummyPicture = new Picture(null, null, false);
+        dummyPicture.setName("Nemo");
+        dummyPictureSet.add(dummyPicture);
 
-		/* create some dummy picture sets and add */
-		pictureSetList.add(new PictureSet("Urlaub", 3));
-		pictureSetList.add(new PictureSet("Golfen", 4));
-		pictureSetList.add(new PictureSet("Weihnachten 2008", 5));
+        Directory dummyDirectory = new Directory();
+        dummyDirectory.setName("Urlaubsbilder");
+        dummyPictureSet.add(dummyDirectory);
 
-		/* create some dummy reports */
-		AbstractReportModel dummyReportOne = new BoxplotModel();
-		dummyReportOne.setReportName("Blendenanalyse");
-		dummyReportOne.setReportDescription("Analyse über Blenden");
+        dummyPictureSet.add(new PictureSet("Grillfest", 10));
+        dummyPictureSet.add(new PictureSet("SuperGoldfische", 11));
 
-		AbstractReportModel dummyReportTwo = new BoxplotModel();
-		dummyReportTwo.setReportName("neue Blendenanalyse");
-		dummyReportTwo.setReportDescription("Analyse über Blenden - neu");
+        /* add to list */
+        pictureSetList.add(dummyPictureSet);
 
-		reportList.add(dummyReportOne);
-		reportList.add(dummyReportTwo);
+        /* create some dummy picture sets and add */
+        pictureSetList.add(new PictureSet("Urlaub", 3));
+        pictureSetList.add(new PictureSet("Golfen", 4));
+        pictureSetList.add(new PictureSet("Weihnachten 2008", 5));
 
-		/* create the model */
-		ProjectModel projectModel = ProjectModel.getInstance(project, pictureSetList, reportList);
+        /* create some dummy reports */
+        AbstractReportModel dummyReportOne = new BoxplotModel();
+        dummyReportOne.setReportName("Blendenanalyse");
+        dummyReportOne.setReportDescription("Analyse über Blenden");
 
-		/* return the model */
-		return projectModel;
-	}
+        AbstractReportModel dummyReportTwo = new BoxplotModel();
+        dummyReportTwo.setReportName("neue Blendenanalyse");
+        dummyReportTwo.setReportDescription("Analyse über Blenden - neu");
 
-	/**
-	 * Writes the project to a project configuration file.
-	 * 
-	 * @param project
-	 *            the project to save.
-	 */
-	public static void writeProjectToFile(final ProjectModel project) {
-	}
+        reportList.add(dummyReportOne);
+        reportList.add(dummyReportTwo);
 
-	private static void manipulateFileNameAndID(ProjectEntry toManipulate) {
-	}
+        /* create the model */
+        ProjectModel projectModel = ProjectModel.getInstance(project, pictureSetList, reportList);
 
-	/**
-	 * Copies a given project
-	 * 
-	 * @param toCopy
-	 *            the project to copy.
-	 * @param the
-	 *            project name for the new project.
-	 */
-	public static ProjectEntry copyProject(final ProjectEntry toCopy, final String projectName) {
-		ProjectEntry newProject = new ProjectEntry(toCopy, projectName);
-		return newProject;
-	}
+        /* return the model */
+        return projectModel;
+    }
+
+    /**
+     * Writes the project to a project configuration file.
+     * 
+     * @param project
+     *            the project to save.
+     */
+    public static void writeProjectToFile(final ProjectModel project) {
+    }
+
+    private static void manipulateFileNameAndID(ProjectEntry toManipulate) {
+    }
+
+    /**
+     * Copies a given project
+     * 
+     * @param toCopy
+     *            the project to copy.
+     * @param the
+     *            project name for the new project.
+     */
+    public static ProjectEntry copyProject(final ProjectEntry toCopy, final String projectName) {
+        ProjectEntry newProject = new ProjectEntry(toCopy, projectName);
+        return newProject;
+    }
 }
