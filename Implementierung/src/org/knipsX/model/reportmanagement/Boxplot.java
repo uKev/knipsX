a002fbaa70457ddefd89ec49c6446838b88e5cc0
Kevin Zuber <ukev@knet.eu>
2010-01-25 18:07:00 +0100

    Worked on Boxplot, most time spent on investigation

--- Implementierung/src/org/knipsX/model/reportmanagement/Boxplot.java
+++ Implementierung/src/org/knipsX/model/reportmanagement/Boxplot.java
@@ -2,100 +2,110 @@ package org.knipsX.model.reportmanagement;
 
 import org.knipsX.model.picturemanagement.PictureContainer;
 
-
 /**
- * Represents the boxplot with all parts containing to it and calculate them. 
- * This is: 
+ * Represents the boxplot with all parts containing to it and calculate them.
+ * This is:
  * mean, median, upperQuartile, lowerQuartile, upperWhisker, lowerWhisker, some outlier, maximum Value, minimum Value
  * All calculation which is needed to genereate this parts will happen here.
+ * 
  * @author Kevin Zuber
- *
+ * 
  */
 public class Boxplot {
 
-	double mean;
-	double median;
-	double upperQuartile;
-	double lowerQuartile;
-	double upperWhisker;
-	double lowerWhisker;
-	double outlier[];
-	double maxValue;
-	double minValue;
-	
-	public Boxplot() {
-	}
-	
-	/**
-	 * Calculates the boxplot from the pictures
-	 * @param pictures the pictures which will be represented with the boxplot
-	 */
-	public Boxplot(PictureContainer pictures){
-		// calculate the Boxplot from the pictures in the pictureSet
-	}
-	
-	/**
-	 * Generates a Boxplot with all parameters. Should only used for testing.
-	 * @param mean
-	 * @param median
-	 * @param upperQuartile
-	 * @param lowerQuartile
-	 * @param upperWhisker
-	 * @param lowerWhisker
-	 * @param outlier
-	 * @param maxValue
-	 * @param minValue
-	 * @param pictureSetName
-	 */
-	public Boxplot(double mean, double median, double upperQuartile,
-			double lowerQuartile, double upperWhisker, double lowerWhisker,
-			double[] outlier, double maxValue, double minValue,
-			String pictureSetName) {
-		this.mean = mean;
-		this.median = median;
-		this.upperQuartile = upperQuartile;
-		this.lowerQuartile = lowerQuartile;
-		this.upperWhisker = upperWhisker;
-		this.lowerWhisker = lowerWhisker;
-		this.outlier = outlier;
-		this.maxValue = maxValue;
-		this.minValue = minValue;
-		PictureSetName = pictureSetName;
-	}
-	
-	public double getMaxValue() {
-		return maxValue;
-	}
-	public double getMinValue() {
-		return minValue;
-	}
-	String PictureSetName;
-	
-	
-	public String getPictureSetName() {
-		return PictureSetName;
-	}
-	public double getMean() {
-		return mean;
-	}
-	public double getMedian() {
-		return median;
-	}
-	public double getUpperQuartile() {
-		return upperQuartile;
-	}
-	public double getLowerQuartile() {
-		return lowerQuartile;
-	}
-	public double getUpperWhisker() {
-		return upperWhisker;
-	}
-	public double getLowerWhisker() {
-		return lowerWhisker;
-	}
-	public double[] getOutlier() {
-		return outlier;
-	}
-	
-	
+    double mean;
+    double median;
+    double upperQuartile;
+    double lowerQuartile;
+    double upperWhisker;
+    double lowerWhisker;
+    double outlier[];
+    double maxValue;
+    double minValue;
+
+    String PictureSetName;
+
+    public Boxplot() {
+    }
+
+    /**
+     * Generates a Boxplot with all parameters. Should only used for testing.
+     * 
+     * @param mean
+     * @param median
+     * @param upperQuartile
+     * @param lowerQuartile
+     * @param upperWhisker
+     * @param lowerWhisker
+     * @param outlier
+     * @param maxValue
+     * @param minValue
+     * @param pictureSetName
+     */
+    public Boxplot(final double mean, final double median, final double upperQuartile, final double lowerQuartile,
+            final double upperWhisker, final double lowerWhisker, final double[] outlier, final double maxValue,
+            final double minValue, final String pictureSetName) {
+        this.mean = mean;
+        this.median = median;
+        this.upperQuartile = upperQuartile;
+        this.lowerQuartile = lowerQuartile;
+        this.upperWhisker = upperWhisker;
+        this.lowerWhisker = lowerWhisker;
+        this.outlier = outlier;
+        this.maxValue = maxValue;
+        this.minValue = minValue;
+        this.PictureSetName = pictureSetName;
+    }
+
+    /**
+     * Calculates the boxplot from the pictures
+     * 
+     * @param pictures
+     *            the pictures which will be represented with the boxplot
+     */
+    public Boxplot(final PictureContainer pictures) {
+        // calculate the Boxplot from the pictures in the pictureSet
+        // add private methods to calculate the stuff
+    }
+
+    public double getLowerQuartile() {
+        return this.lowerQuartile;
+    }
+
+    public double getLowerWhisker() {
+        return this.lowerWhisker;
+    }
+
+    public double getMaxValue() {
+        return this.maxValue;
+    }
+
+    public double getMean() {
+        return this.mean;
+    }
+
+    public double getMedian() {
+        return this.median;
+    }
+
+    public double getMinValue() {
+        return this.minValue;
+    }
+
+    public double[] getOutlier() {
+        return this.outlier;
+    }
+
+    public String getPictureSetName() {
+        return this.PictureSetName;
+    }
+
+    public double getUpperQuartile() {
+        return this.upperQuartile;
+    }
+
+    public double getUpperWhisker() {
+        return this.upperWhisker;
+    }
+
 }
