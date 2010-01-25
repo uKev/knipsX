8a2016d54fd56f9f62edd0a0f244793cfcaf3884
Kevin Zuber <ukev@knet.eu>
2010-01-25 18:05:53 +0100

    Fixed another little bug for the great java3David

--- Implementierung/src/org/knipsX/model/reportmanagement/Cluster3DModel.java
+++ Implementierung/src/org/knipsX/model/reportmanagement/Cluster3DModel.java
@@ -8,23 +8,30 @@ import org.knipsX.model.picturemanagement.PictureContainer;
  * Represents the 3D-cluser and allocate the axes to it.
  * 
  * @author Kevin Zuber
- *
+ * 
  */
-public class Cluster3DModel extends AbstractTrippleAxesModel{
+public class Cluster3DModel extends AbstractTrippleAxesModel {
 
-	private Frequency3DPoint [] frequency3DPoints;
-	
-	public Cluster3DModel(ArrayList<PictureContainer> pictureContainer, Axis xAxis, Axis zAxis, Axis yAxis) {
-		super(pictureContainer, xAxis, zAxis, yAxis);
-	}
-	
-	public Cluster3DModel() {
-		super();
-	}
-	
-	public Frequency3DPoint [] getFrequency3DPoints (){
-		return this.frequency3DPoints;
-	}
+    private Frequency3DPoint[] frequency3DPoints;
+
+    public Cluster3DModel(ArrayList<PictureContainer> pictureContainer, Axis xAxis, Axis zAxis, Axis yAxis) {
+        super(pictureContainer, xAxis, zAxis, yAxis);
+    }
+
+    public Cluster3DModel() {
+        super();
+    }
+
+    public Frequency3DPoint[] getFrequency3DPoints() {
+        return this.frequency3DPoints;
+    }
+
+    @Override
+    public PictureParameter[] getPicturesWithMissingExifParameter() {
+        // TODO Auto-generated method stub
+        // kann das in die Axen-Klassen ausgelagert werden? Gibt es da Unterschiede?
+        return null;
+    }
 
     @Override
     public Object getMaxX() {
@@ -46,24 +53,6 @@ public class Cluster3DModel extends AbstractTrippleAxesModel{
 
     @Override
     public Object getMinY() {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-    @Override
-    public Axis getxAxis() {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-    @Override
-    public void setxAxis(Axis xAxis) {
-        // TODO Auto-generated method stub
-        
-    }
-
-    @Override
-    public PictureParameter[] getPicturesWithMissingExifParameter() {
         // TODO Auto-generated method stub
         return null;
     }
