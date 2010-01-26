4847203bf0cfe41aaafb08c363dbd7f7e34468d8
David Kaufman <david.kaufman@gmx.de>
2010-01-26 13:34:42 +0100

    Am JBoxplot gearbeitet. So gut wie fertig

--- Implementierung/src/org/knipsX/view/diagrams/JBoxplot.java
+++ Implementierung/src/org/knipsX/view/diagrams/JBoxplot.java
@@ -4,8 +4,6 @@ import javax.vecmath.Vector3d;
 
 import org.knipsX.model.reportmanagement.AbstractReportModel;
 import org.knipsX.model.reportmanagement.Boxplot;
-import org.knipsX.model.reportmanagement.BoxplotModel;
-
 /**
  * This class implements how the BoxplotModel is to be drawn.
  * 
@@ -37,44 +35,82 @@ public class JBoxplot<M extends AbstractReportModel> extends JAbstract2DDiagram<
         this.showGrid = false;
         
         //Boxplot boxplot = new Boxplot(0.05490246, 0.0775161, 0.7477903, -0.5993204, 2.797647, -2.462955, null, 2.797647, -2.462955, "BLA");
-        Boxplot boxplot = new Boxplot(0.2784033, 0.0775161, 12.31617, -15.35961 , 73.23535, -46.98489, null, 80,  -80, "BLA");
+        double[] outlier = {75, 76, 85, 90};
+        Boxplot boxplot = new Boxplot(0.2784033, 10, 12.31617, -25 , 73.23535, -46.98489, outlier, 90,  -50, "BLA");
+
+        
         double whiskerScale = 0.05;
         double whiskerScaleWidth = 1;
+        double boxWidth = 0.5;
         
         
         double range = Math.abs(boxplot.getMaxValue()) + Math.abs(boxplot.getMinValue());
         double scaleFactor = this.axis3D[0].getAxisSize() / range;
+        
+        
         double interquartileRange = Math.abs(boxplot.getUpperQuartile()) + Math.abs(boxplot.getLowerQuartile());
+        double middleOfinterQuartileRange = getMiddleOfReportSpace(boxplot.getLowerQuartile(), boxplot.getUpperQuartile());
+       
+        /* Create inner quartile range*/
+        this.createCube(new Vector3d(0, getAxisSpace(middleOfinterQuartileRange, boxplot.getMinValue(), boxplot.getMaxValue()), this.axis3D[1].getAxisSize() / 2), new Vector3d(boxWidth, (interquartileRange / 2) * scaleFactor , boxWidth), this.basicMaterial(0.125f, 0.125f, 0.125f));        
+       
+        
+        double middleOfUpperWhiskerAndUpperQuartile = getMiddleOfReportSpace(boxplot.getUpperQuartile(), boxplot.getUpperWhisker());
         double upperWhiskerRange = Math.abs(boxplot.getUpperWhisker()) - Math.abs(boxplot.getUpperQuartile());
+        
+        /* Create upper whisker */
+        this.createCube(new Vector3d(0, getAxisSpace(middleOfUpperWhiskerAndUpperQuartile, boxplot.getMinValue(), boxplot.getMaxValue()), this.axis3D[1].getAxisSize() / 2), new Vector3d(whiskerScale, (upperWhiskerRange / 2) * scaleFactor , whiskerScale), this.basicMaterial(0.625f, 0.125f, 0.125f));
+        this.createCube(new Vector3d(0, getAxisSpace(boxplot.getUpperWhisker(), boxplot.getMinValue(), boxplot.getMaxValue()), this.axis3D[1].getAxisSize() / 2), new Vector3d(whiskerScaleWidth, whiskerScale , whiskerScale), this.basicMaterial(0.625f, 0.125f, 0.625f));
+        
+        
+        double middleOfLowerWhiskerAndLowerQuartile = getMiddleOfReportSpace(boxplot.getLowerQuartile(), boxplot.getLowerWhisker());
         double lowerWhiskerRange = Math.abs(boxplot.getLowerWhisker()) - Math.abs(boxplot.getLowerQuartile());
         
-//        double interquartileRangeBottom = Math.abs(boxplot.getLowerWhisker()) - Math.abs(boxplot.getLowerQuartile());
-//        double interquartileRangeMiddle = Math.abs(boxplot.getLowerWhisker()) - Math.abs(boxplot.getLowerQuartile()) + (interquartileRange / 2.0);
-//        double interquartileRangeTop = Math.abs(boxplot.getLowerWhisker()) - Math.abs(boxplot.getLowerQuartile()) + interquartileRange;        
-//        double upperWhiskerRangeMiddle = (upperWhiskerRange / 2.0) + interquartileRangeTop;        
-//        double lowerWhiskerRangeMiddle = (lowerWhiskerRange / 2.0);
-//        
-//        /*z-Axis y-Axis x-Axis*/
-//        /* Create main box */
-//        this.createCube(new Vector3d(0, interquartileRangeMiddle * scaleFactor, this.axis3D[1].getAxisSize() / 2.0), new Vector3d(0.5, scaleFactor * (interquartileRange / 2.0), 0.5), this.basicMaterial(0.125f, 0.125f, 0.125f));
-//        
-//        /* Create upper whisker */
-//        this.createCube(new Vector3d(0, upperWhiskerRangeMiddle * scaleFactor, this.axis3D[1].getAxisSize() / 2.0), new Vector3d(whiskerScale , scaleFactor * (upperWhiskerRange / 2.0), whiskerScale), this.basicMaterial(0.725f, 0.125f, 0.125f));
-//        this.createCube(new Vector3d(0, (interquartileRangeTop + upperWhiskerRange) * scaleFactor, this.axis3D[1].getAxisSize() / 2.0), new Vector3d(whiskerScaleWidth , whiskerScale, whiskerScale), this.basicMaterial(0.725f, 0.125f, 0.125f));
-//        
-//        
-//        /* Create lower whisker */
-//        this.createCube(new Vector3d(0, lowerWhiskerRangeMiddle * scaleFactor, this.axis3D[1].getAxisSize() / 2.0), new Vector3d(whiskerScale , scaleFactor * (lowerWhiskerRange / 2.0), whiskerScale), this.basicMaterial(0.725f, 0.125f, 0.125f));
-//        this.createCube(new Vector3d(0, (interquartileRangeBottom - lowerWhiskerRange) * scaleFactor, this.axis3D[1].getAxisSize() / 2.0), new Vector3d(whiskerScaleWidth , whiskerScale, whiskerScale), this.basicMaterial(0.725f, 0.125f, 0.125f));
+        /* Create lower whisker */
+        this.createCube(new Vector3d(0, getAxisSpace(middleOfLowerWhiskerAndLowerQuartile, boxplot.getMinValue(), boxplot.getMaxValue()), this.axis3D[1].getAxisSize() / 2), new Vector3d(whiskerScale, (lowerWhiskerRange / 2) * scaleFactor , whiskerScale), this.basicMaterial(0.625f, 0.125f, 0.125f));
+        this.createCube(new Vector3d(0, getAxisSpace(boxplot.getLowerWhisker(), boxplot.getMinValue(), boxplot.getMaxValue()), this.axis3D[1].getAxisSize() / 2), new Vector3d(whiskerScaleWidth, whiskerScale , whiskerScale), this.basicMaterial(0.625f, 0.125f, 0.625f));
+        
+        
+        /* Create mean */
+        this.createSphere(new Vector3d(-boxWidth, getAxisSpace(boxplot.getMean(), boxplot.getMinValue(), boxplot.getMaxValue()), this.axis3D[1].getAxisSize() / 2), new Vector3d(boxWidth / 4, boxWidth / 4 , whiskerScale), this.basicMaterial(1, 0.125f, 0.125f));
 
+        /* Create median */
+        this.createCube(new Vector3d(-boxWidth, getAxisSpace(boxplot.getMedian(), boxplot.getMinValue(), boxplot.getMaxValue()), this.axis3D[1].getAxisSize() / 2), new Vector3d(boxWidth, whiskerScale , whiskerScale), this.basicMaterial(1, 0.125f, 0.125f));
+        
+        
+        /* Create outliers */
+        if (boxplot.getOutlier() != null) {
+            for (int i = 0; i < boxplot.getOutlier().length; i++) {
+                this.createSphere(new Vector3d(0, getAxisSpace(boxplot.getOutlier()[i], boxplot.getMinValue(), boxplot.getMaxValue()), this.axis3D[1].getAxisSize() / 2), new Vector3d(boxWidth / 5, boxWidth / 5 , whiskerScale), this.basicMaterial(1, 1, 1));
+            }
+        }
+        
         this.axis3D[0].generateSegmentDescription(boxplot.getMinValue(), boxplot.getMaxValue(), 8);
         this.axis3D[1].generateSegmentDescription(null, null, 2);
-        
-        
-     
-        
-        
 
     }
 
+    
+    private double getAxisSpace(double reportSpace, double minReportSpace, double maxReportSpace) {
+        double range = Math.abs(maxReportSpace) + Math.abs(minReportSpace);
+        double slope = Math.abs(axis3D[0].getAxisSize()) / range;
+        double yIntercept = axis3D[0].getAxisSize() - slope * maxReportSpace;
+        
+        /* m * x + c */
+        return slope * reportSpace + yIntercept;
+        
+    }
+    
+    private double getMiddleOfReportSpace(double minReportSpace, double maxReportSpace) {
+        
+        double range = Math.max(maxReportSpace, minReportSpace) - Math.min(maxReportSpace, minReportSpace); 
+        
+        if (Math.min(maxReportSpace, minReportSpace) < 0) {
+            double tempRange = Math.max(maxReportSpace, minReportSpace) - Math.min(maxReportSpace, minReportSpace);
+            return Math.min(maxReportSpace, minReportSpace) + tempRange / 2;
+        }
+
+        return Math.max(minReportSpace, maxReportSpace) - (range / 2);        
+    }
+    
 }
