e513821918eca388e1da883e0a4ced144541e9ff
BenniX47 <benni-borstel@online.de>
2010-01-28 22:20:04 +0100

    Kleine Verbesserungen an Picture und Pictureset.
    Signed-off-by: BenniX47 <benni-borstel@online.de>

--- Implementierung/src/org/knipsX/model/picturemanagement/Picture.java
+++ Implementierung/src/org/knipsX/model/picturemanagement/Picture.java
@@ -132,7 +132,7 @@ public class Picture implements PictureContainer, ImageObserver {
             try {
                 this.bigThumbnail = Picture.getThumbOf(ImageIO.read(pictureFile), 200, Image.SCALE_FAST);
             } catch (IOException e) {
-                System.err.println("[Picture::getBigThumbnail()] - File didn't exist - "
+                System.err.println("[Picture::getBigThumbnail()] - File does not exist - "
                         + pictureFile.getAbsolutePath());
             }
         }
@@ -141,7 +141,7 @@ public class Picture implements PictureContainer, ImageObserver {
             try {
                 this.smallThumbnail = Picture.getThumbOf(ImageIO.read(pictureFile), 50, Image.SCALE_FAST);
             } catch (IOException e) {
-                System.err.println("[Picture::getSmallThumbnail()] - File didn't exist - "
+                System.err.println("[Picture::getSmallThumbnail()] - File does not exist - "
                         + pictureFile.getAbsolutePath());
             }
         }
