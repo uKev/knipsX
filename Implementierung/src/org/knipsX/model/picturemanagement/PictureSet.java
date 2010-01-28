433bdeaf0ea2da74a423ddcee48ba9bb399bcfc1
Live session user <ubuntu@ubuntu.(none)>
2010-01-28 12:42:23 +0100

    Iterator der PictureContainer geben nun nur noch Picture zurück und PictureContainer implementiert das Interface Iterable.

--- Implementierung/src/org/knipsX/model/picturemanagement/PictureSet.java
+++ Implementierung/src/org/knipsX/model/picturemanagement/PictureSet.java
@@ -1,91 +1,118 @@
 package org.knipsX.model.picturemanagement;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class PictureSet implements PictureContainer {
 
-    private List<PictureContainer> childs = new ArrayList<PictureContainer>();
-    
-    private List<Picture> inactivePictures = new ArrayList<Picture>();
+	private List<PictureContainer> children = new ArrayList<PictureContainer>();
 
-    private int id;
+	private List<Picture> inactivePictures = new ArrayList<Picture>();
 
-    private int currentPosition;
+	private int id;
 
-    private String name;
+	private int currentChild;
 
-    public PictureSet(String pictureSetName, int freePictureSetID) {
-        this.name = pictureSetName;
-        this.id = freePictureSetID;
-    }
+	private String name;
 
-    public PictureSet(PictureSet pictureSetToCopy, String pictureSetName, int freePictureSetID) {
-        this.name = pictureSetName;
-        this.id = freePictureSetID;
-        this.childs = pictureSetToCopy.getItems();
-    }
+	public PictureSet(String pictureSetName, int freePictureSetID) {
+		this.name = pictureSetName;
+		this.id = freePictureSetID;
+	}
 
-    public boolean addToChilds(PictureContainer container) {
-        if(childs.contains(container)){
-            System.out.println("Element is already in the PictureSet");        
-            return false;
-        }else{
-            if (container instanceof PictureSet){
-                if (this.id == ((PictureSet)container).getID()){
-                    System.out.println("Can´t add PictureSet");
-                    return false;
-                }else {
-                    return childs.add(container);
-                }
-                
-            }else{
-                return childs.add(container); 
-            }
-        }      
-    }
+	public PictureSet(PictureSet pictureSetToCopy, String pictureSetName,
+			int freePictureSetID) {
+		this.name = pictureSetName;
+		this.id = freePictureSetID;
+		this.children = pictureSetToCopy.getItems();
+	}
 
-    public boolean remove(PictureContainer container) {
-        return childs.remove(container);
-    }
+	public boolean addToChilds(PictureContainer container) {
+		if (children.contains(container)) {
+			System.out.println("Element is already in the PictureSet");
+			return false;
+		} else {
+			if (container instanceof PictureSet) {
+				if (this.id == ((PictureSet) container).getID()) {
+					System.out.println("Can´t add PictureSet");
+					return false;
+				} else {
+					return children.add(container);
+				}
 
-    public List<PictureContainer> getItems() {
-        return new ArrayList<PictureContainer>(this.childs);
-    }
+			} else {
+				return children.add(container);
+			}
+		}
+	}
 
-    public boolean hasNext() {
+	public boolean remove(PictureContainer container) {
+		return children.remove(container);
+	}
 
-        /* check if next item is in list */
-        return (currentPosition++) <= (this.childs.size() - 1);
-    }
+	public List<PictureContainer> getItems() {
+		return new ArrayList<PictureContainer>(this.children);
+	}
 
-    public PictureContainer next() {
-        PictureContainer pictureContainer = this.childs.get(currentPosition);
+	public boolean hasNext() {
+		boolean hasNext = false;
 
-        this.currentPosition++;
+		if (this.children.get(currentChild).hasNext()) {
+			hasNext = true;
+		} else {
+			int child = currentChild;
 
-        return pictureContainer;
-    }
+			while (!hasNext && child < this.children.size() - 1) {
+				child++;
+				if (this.children.get(child).hasNext()) {
+					hasNext = true;
+				}
+			}
+		}
+		return hasNext;
+	}
 
-    public void remove() {
-        /* not implemented */
-    }
+	public Picture next() {
+		Picture picture = null;
+		
+		if (this.children.get(currentChild).hasNext()) {
+			picture = this.children.get(currentChild).next();
+		} else {
 
-    public String getName() {
-        return name;
-    }
-    
-    public int getID(){
-        return id;
-    }
+			while (picture == null && currentChild < this.children.size() -1) {
+				this.currentChild++;
+				if (this.children.get(currentChild).hasNext()){
+					picture = this.children.get(currentChild).next();
+				}
+			}
+		}		
+		return picture;
+	}
 
-    public int compareTo(PictureContainer picturesetToCompare) {
-        if (this.id == ((PictureSet) picturesetToCompare).getID()){
-            return 0;
-        } else if (this.getName().compareTo(picturesetToCompare.getName()) == 1) {
-            return 1;
-        } else {
-            return -1; 
-        }
-    }
+	public void remove() {
+		/* not implemented */
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getID() {
+		return id;
+	}
+
+	public Iterator<Picture> iterator() {
+		return this;
+	}
+
+	public int compareTo(PictureContainer picturesetToCompare) {
+		if (this.id == ((PictureSet) picturesetToCompare).getID()) {
+			return 0;
+		} else if (this.getName().compareTo(picturesetToCompare.getName()) == 1) {
+			return 1;
+		} else {
+			return -1;
+		}
+	}
 }
