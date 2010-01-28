08197acf93bea6af96ac9656df5b5b7867c4560d
Kevin Zuber <ukev@knet.eu>
2010-01-28 16:32:35 +0100

    BoxplotModel implemented. Closes #101.

--- Implementierung/src/org/knipsX/model/reportmanagement/BoxplotModel.java
+++ Implementierung/src/org/knipsX/model/reportmanagement/BoxplotModel.java
@@ -2,117 +2,222 @@ package org.knipsX.model.reportmanagement;
 
 import java.util.ArrayList;
 
+import org.knipsX.model.picturemanagement.Picture;
 import org.knipsX.model.picturemanagement.PictureContainer;
 
 /**
  * Model which contain the Boxplots and name them with an Axis.
+ * 
  * @author Kevin Zuber
- *
+ * 
  */
-public class BoxplotModel extends AbstractSingleAxisModel{
+public class BoxplotModel extends AbstractSingleAxisModel {
 
-	private Boxplot [] boxplots;
-	private boolean wilcoxonTestActive;
-	private WilcoxonTestType wilcoxonTestType;
-	private float wilcoxonSignificance;
-	
-	
-	public BoxplotModel() {
-		super();
-	}
-	
-	/**
-	 * Constructor for the Boxplot Model
-	 * @param pictureContainer
-	 * @param xAxis
-	 */
-	public BoxplotModel(ArrayList<PictureContainer> pictureContainer, Axis xAxis) {
-		super(pictureContainer, xAxis);
-		// TODO: Auto-generated constructor stub
-		// TODO: calculate the Stuff and save it to boxplots
-	}
+    private ArrayList<Boxplot> boxplots;
+    private WilcoxonTest wilcoxonTest;
+    private ArrayList<PictureParameter> missingExifParameter;
+    @Deprecated
+    private boolean wilcoxonTestActive;
+    @Deprecated
+    private WilcoxonTestType wilcoxonTestType;
+    @Deprecated
+    private float wilcoxonSignificance;
 
-	public Boxplot[] getBoxplots() {
-		return boxplots;
-	}
-	
-	/**
-	 * @return the result of the Wilcoxon test
-	 */
-	public float getWilcoxonPValue() {
-		// TODO: implement
-		return 0f;
-	}
+    /**
+     * @deprecated deprecated
+     * deprecated
+     */
+    public BoxplotModel() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
 
-	/**
-	 * 
-	 * @return if the wilcoxon test is used
-	 */
-	public boolean isWilcoxonTestActive() {
-		return wilcoxonTestActive;
-	}
+    /**
+     * Constructor for the Boxplot Model
+     * 
+     * @param pictureContainers
+     *            the pictureContainers which are used for the boxplots (one boxplot per pictureContainer)
+     * @param xAxis
+     *            the xAxis of the boxplot
+     */
+    public BoxplotModel(final ArrayList<PictureContainer> pictureContainers, final Axis xAxis) {
+        super(pictureContainers, xAxis);
+        this.boxplots = new ArrayList<Boxplot>(pictureContainers.size());
 
-	public void setWilcoxonTestActive(boolean wilcoxonTestActive) {
-		this.wilcoxonTestActive = wilcoxonTestActive;
-	}
+        for (final PictureContainer pictures : pictureContainers) {
+            String boxplotName;
+            if (xAxis.getDescription() == null) {
+                boxplotName = xAxis.getParameter().toString();
+            } else {
+                boxplotName = xAxis.getDescription();
+            }
+            this.boxplots.add(new Boxplot(pictures, xAxis.getParameter(), boxplotName));
+        }
+        this.wilcoxonTest = new WilcoxonTest(pictureContainers, xAxis.getParameter());
 
-	public WilcoxonTestType getWilcoxonTestType() {
-		return wilcoxonTestType;
-	}
+        this.missingExifParameter = null;
 
-	public void setWilcoxonTestType(WilcoxonTestType wilcoxonTestType) {
-		this.wilcoxonTestType = wilcoxonTestType;
-	}
+        for (final PictureContainer pictureContainer : pictureContainers) {
+            for (final Picture picture : pictureContainer) {
+                if (picture.getExifParameter(xAxis.getParameter()) == null) {
+                    this.missingExifParameter.add(new PictureParameter(xAxis.getParameter(), picture));
+                }
+            }
+        }
 
-	public float getWilcoxonSignificance() {
-		return wilcoxonSignificance;
-	}
+    }
 
-	public void setWilcoxonSignificance(float wilcoxonSignificance) {
-		this.wilcoxonSignificance = wilcoxonSignificance;
-	}
+    /**
+     * Getter for the boxplots
+     * 
+     * @return boxplots
+     */
+    public ArrayList<Boxplot> getBoxplots() {
+        return this.boxplots;
+    }
 
+    /**
+     * getter for the maximum x-value the boxplots
+     * 
+     * @return the count of the boxplots
+     */
     @Override
     public Object getMaxX() {
-        // TODO Auto-generated method stub
-        return null;
+
+        return this.boxplots.size();
     }
 
     @Override
+    /**
+     * return the maximum Y Value, return Double.MIN_VALUE if there are no boxplots
+     */
     public Object getMaxY() {
-        // TODO Auto-generated method stub
-        return null;
+        Double maxY = Double.MIN_VALUE;
+        for (final Boxplot boxplot : this.boxplots) {
+            if (maxY < boxplot.getMaxValue()) {
+                maxY = boxplot.getMaxValue();
+            }
+        }
+        return maxY;
     }
 
     @Override
+    /**
+     * return 0 because there are no negative boxplots
+     */
     public Object getMinX() {
-        // TODO Auto-generated method stub
-        return null;
+        return 0;
     }
 
     @Override
+    /**
+     * return the minimum Y Value, return  Double.MAX_VALUE if there are nox boxplots
+     */
     public Object getMinY() {
-        // TODO Auto-generated method stub
-        return null;
+        Double minY = Double.MAX_VALUE;
+        for (final Boxplot boxplot : this.boxplots) {
+            if (minY > boxplot.getMinValue()) {
+                minY = boxplot.getMinValue();
+            }
+        }
+        return minY;
     }
 
     @Override
-    public Axis getxAxis() {
-        // TODO Auto-generated method stub
-        return null;
+    public ArrayList<PictureParameter> getPicturesWithMissingExifParameter() {
+        return this.missingExifParameter;
     }
 
-    @Override
-    public void setxAxis(Axis xAxis) {
-        // TODO Auto-generated method stub
-        
+    /**
+     * deprecated
+     * 
+     * @deprecated Wilcoxon Test is externalized, use getWilcoxonTest()
+     * @return the result of the Wilcoxon test
+     */
+    @Deprecated
+    public float getWilcoxonPValue() {
+        // TODO: remove
+        return 0f;
     }
 
-    @Override
-    public PictureParameter[] getPicturesWithMissingExifParameter() {
-        // TODO Auto-generated method stub
-        return null;
+    /**
+     * deprecated
+     * 
+     * @deprecated Wilcoxon Test is externalized
+     * @return deprecated
+     */
+    @Deprecated
+    public float getWilcoxonSignificance() {
+        return this.wilcoxonSignificance;
     }
-	
-	
+
+    /**
+     * Getter for the WilcoxonTest.
+     * 
+     * @return the wilcoxonTest. Returns null if wilcoxonTest is not active.
+     */
+    public WilcoxonTest getWilcoxonTest() {
+        WilcoxonTest wilcoxonTest = this.wilcoxonTest;
+        if (wilcoxonTest != null) {
+            if (wilcoxonTest.isValid()) {
+                wilcoxonTest = null;
+            }
+        }
+        return wilcoxonTest;
+    }
+
+    /**
+     * deprecated
+     * 
+     * @deprecated Wilcoxon Test is externalized
+     * @return deprecated
+     */
+    @Deprecated
+    public WilcoxonTestType getWilcoxonTestType() {
+        return this.wilcoxonTestType;
+    }
+
+    /**
+     * deprecated
+     * 
+     * @deprecated deprecated
+     * @return if the wilcoxon test is used
+     */
+    @Deprecated
+    public boolean isWilcoxonTestActive() {
+        return this.wilcoxonTestActive;
+    }
+
+    /**
+     * deprecated
+     * 
+     * @deprecated Wilcoxon Test is externalized
+     * @param wilcoxonSignificance
+     *            deprecated
+     */
+    @Deprecated
+    public void setWilcoxonSignificance(final float wilcoxonSignificance) {
+        this.wilcoxonSignificance = wilcoxonSignificance;
+    }
+
+    /**
+     * deprecated
+     * @deprecated
+     *             deprecated
+     * @param wilcoxonTestActive deprecated
+     */
+    public void setWilcoxonTestActive(final boolean wilcoxonTestActive) {
+        this.wilcoxonTestActive = wilcoxonTestActive;
+    }
+
+    /**
+     * @deprecated Wilcoxon Test is externalized
+     * @param wilcoxonTestType
+     *            deprecated
+     */
+    @Deprecated
+    public void setWilcoxonTestType(final WilcoxonTestType wilcoxonTestType) {
+        this.wilcoxonTestType = wilcoxonTestType;
+    }
+
 }
