17171fdfe1e0787a506d94b2fdeb474d18fa62b7
clyde <clyde_swt@gmx.de>
2010-01-29 20:42:58 +0100

    XML update closes #92

--- Implementierung/src/test/hello/Hello.java
+++ Implementierung/src/test/hello/Hello.java
@@ -1,13 +1,14 @@
 import java.util.*;
 import javax.xml.bind.*;
 import hello.*;
+import java.io.*;
 
 public class Hello {
 
     private ObjectFactory of;
     private GreetingListType grList;
 
-    public Hello(){
+    public Hello() {
         of = new ObjectFactory();
         grList = of.createGreetingListType();
     }
@@ -25,7 +26,23 @@ public class Hello {
                 of.createGreetings( grList );
             JAXBContext jc = JAXBContext.newInstance( "hello" );
             Marshaller m = jc.createMarshaller();
-            m.marshal( gl, System.out );
+	    m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT ,
+                   new Boolean(true));
+ 
+		m.marshal( gl, System.out );
+		System.out.println("\n 2nd output:\n");
+                m.marshal( gl, System.out );
+
+		try{
+
+		OutputStream os = new FileOutputStream( "output.xml" );
+		System.out.println("Writing to file...\n");
+		m.marshal( gl, os );
+
+		}catch (Exception e){//Catch exception if any
+      		System.err.println("Error: " + e.getMessage());
+		}
+		System.out.println("Done.\n");
         } catch( JAXBException jbe ){
             // ...
         }
