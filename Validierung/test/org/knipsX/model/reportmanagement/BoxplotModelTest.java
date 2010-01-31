91ca3eb11caed37a760af30c1d4e67e83bc24c43
Kevin Zuber <uKev@knet.eu>
2010-01-31 23:59:20 +0100

    BoxplotModelTest hinzugefügt

---
+++ Validierung/test/org/knipsX/model/reportmanagement/BoxplotModelTest.java
@@ -0,0 +1,120 @@
+/**
+ * 
+ */
+package org.knipsX.model.reportmanagement;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author kevin
+ *
+ */
+public class BoxplotModelTest {
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+    }
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception {
+    }
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    /**
+     * Test method for {@link org.knipsX.model.reportmanagement.BoxplotModel#getMaxX()}.
+     */
+    @Test
+    public void testGetMaxX() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link org.knipsX.model.reportmanagement.BoxplotModel#getMaxY()}.
+     */
+    @Test
+    public void testGetMaxY() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link org.knipsX.model.reportmanagement.BoxplotModel#getMinX()}.
+     */
+    @Test
+    public void testGetMinX() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link org.knipsX.model.reportmanagement.BoxplotModel#getMinY()}.
+     */
+    @Test
+    public void testGetMinY() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link org.knipsX.model.reportmanagement.BoxplotModel#setxAxis(org.knipsX.model.reportmanagement.Axis)}.
+     */
+    @Test
+    public void testSetxAxis() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link org.knipsX.model.reportmanagement.BoxplotModel#BoxplotModel()}.
+     */
+    @Test
+    public void testBoxplotModel() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link org.knipsX.model.reportmanagement.BoxplotModel#BoxplotModel(java.util.ArrayList, org.knipsX.model.reportmanagement.Axis)}.
+     */
+    @Test
+    public void testBoxplotModelArrayListOfPictureContainerAxis() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link org.knipsX.model.reportmanagement.BoxplotModel#getBoxplots()}.
+     */
+    @Test
+    public void testGetBoxplots() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link org.knipsX.model.reportmanagement.BoxplotModel#getWilcoxonTest()}.
+     */
+    @Test
+    public void testGetWilcoxonTest() {
+        fail("Not yet implemented");
+    }
+
+}
