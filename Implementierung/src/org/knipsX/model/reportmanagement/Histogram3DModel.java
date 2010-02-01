a482f72e307e2ec621f388c5a12ab04ecba851a8
Kevin Zuber <ukev@knet.eu>
2010-02-01 15:35:42 +0100

    Histogram3DModel implementiert. Closes #96

--- Implementierung/src/org/knipsX/model/reportmanagement/Histogram3DModel.java
+++ Implementierung/src/org/knipsX/model/reportmanagement/Histogram3DModel.java
@@ -2,75 +2,145 @@ package org.knipsX.model.reportmanagement;
 
 import java.util.ArrayList;
 
+import org.knipsX.model.picturemanagement.Picture;
 import org.knipsX.model.picturemanagement.PictureContainer;
 
 /**
  * The model auf the Histogram3D which calculate the statistic categories and allocate the axes to the Data.
+ * 
  * @author Kevin Zuber
- *
+ * 
  */
 
-public class Histogram3DModel extends AbstractDoubleAxesModel{
+public class Histogram3DModel extends AbstractDoubleAxesModel {
 
-	private Category categories[];
-	
-	public Histogram3DModel(ArrayList<PictureContainer> pictureContainer,
-			Axis xAxis, Axis zAxis) {
-		super(pictureContainer, xAxis, zAxis);
-		// TODO Auto-generated constructor stub
-	}	
-	
-	public Histogram3DModel() {
-		super();
-	}
-	
-	/*
-	 * Berechnet die Statistik-Klassen und liefert die entsprechenden Objekte zurück
-	 */
-	public Category [] getCategories(){
-		return this.categories;
-		
-	}
+    private Category[][] categories;
+    private double zCategorySize;
+    private double xCategorySize;
 
-    @Override
-    public double getMaxX() {
-        // TODO Auto-generated method stub
-        return 0;
+    public Histogram3DModel(ArrayList<PictureContainer> pictureContainer, Axis xAxis, Axis zAxis) {
+        super(pictureContainer, xAxis, zAxis);
+
     }
 
-    @Override
-    public double getMaxY() {
-        // TODO Auto-generated method stub
-        return 0;
+    public Histogram3DModel() {
+        super();
     }
 
-    @Override
-    public double getMinX() {
-        // TODO Auto-generated method stub
-        return 0;
+    /*
+     * Berechnet die Statistik-Klassen und liefert die entsprechenden Objekte zurück
+     */
+    public Category[][] getCategories() {
+
+        calculateIfNeeded();
+        return this.categories;
+
     }
 
-    @Override
-    public double getMinY() {
-        // TODO Auto-generated method stub
-        return 0;
+    private void calculateIfNeeded() {
+        if (!this.isDataCalculated()) {
+
+            allocatePicturesToCategories();
+            this.dataIsCalculated(true);
+        }
+
     }
 
-    @Override
-    public Axis getxAxis() {
-        // TODO Auto-generated method stub
-        return null;
+    private void calculateExtremeValues() {
+        this.maxX = Double.MIN_VALUE;
+        this.maxY = Double.MIN_VALUE;
+        this.maxZ = Double.MIN_VALUE;
+        this.minX = Double.MAX_VALUE;
+        this.minY = Double.MAX_VALUE;
+        this.minZ = Double.MAX_VALUE;
+
+        for (PictureContainer pictureContainer : this.getPictureContainer()) {
+            for (Picture picture : pictureContainer) {
+
+                // TWEAK: allow other types than double
+                Double xValue = ((Float) picture.getExifParameter(this.getxAxis().getParameter())).doubleValue();
+                Double zValue = ((Float) picture.getExifParameter(this.getzAxis().getParameter())).doubleValue();
+
+                if (this.maxX < xValue) {
+                    maxX = xValue;
+                }
+                if (this.maxZ < zValue) {
+                    maxZ = zValue;
+                }
+                if (this.minX > xValue) {
+                    minX = xValue;
+                }
+                if (this.minZ > zValue) {
+                    minZ = zValue;
+                }
+
+            }
+        }
+
     }
 
-    @Override
-    public void setxAxis(Axis xAxis) {
-        // TODO Auto-generated method stub
+    private void allocatePicturesToCategories() {
+        generateCategories();
+        Double xValue;
+        Double zValue;
+        Category category;
+        int barCount;
         
-    }
+        for (PictureContainer pictureContainer : this.getPictureContainer()) {
+            /*
+             * create a bar for each pictureContainer in each Category
+             */
+            /*
+             * each x coordinate
+             */
+            for (int i = 0; i < this.categories.length; i++) {
+                /*
+                 * each z coordinate
+                 */
+                for (int j = 0; j < this.categories[i].length; j++) {
 
-    @Override
-    public ArrayList<PictureParameter> getPicturesWithMissingExifParameter() {
-        // TODO Auto-generated method stub
-        return null;
+                    for (Picture picture : pictureContainer) {
+                        barCount = 0;
+                        xValue = ((Float) picture.getExifParameter(this.getxAxis().getParameter())).doubleValue();
+                        zValue = ((Float) picture.getExifParameter(this.getzAxis().getParameter())).doubleValue();
+                        category = categories[i][j];
+
+                        if (xValue < category.getMaxValueX() && xValue >= category.getMinValueX()
+                                && zValue < category.getMaxValueZ() && zValue >= category.getMinValueZ()) {
+                            /* 
+                             * Picture fits in the category, yeah!
+                             */
+                            barCount++;
+                        }
+                        category.addBar(new Bar(pictureContainer, barCount));
+                    }
+                }
+            }
+        }
+    }
+    
+    private void generateCategories() {
+        calculateExtremeValues();
+        int numberOfCategories = 5;
+
+        this.categories = new Category[numberOfCategories][numberOfCategories];
+
+        this.xCategorySize = (Math.abs(this.maxX - this.minX) / numberOfCategories);
+        this.zCategorySize = (Math.abs(this.maxX - this.minX) / numberOfCategories);
+        double minValueX;
+        double maxValueX;
+        double minValueZ;
+        double maxValueZ;
+
+        for (int i = 0; i < this.categories.length; i++) {
+            for (int j = 0; j < this.categories[i].length; j++) {
+                minValueX = this.minX + i * xCategorySize;
+                maxValueX = minValueX + xCategorySize;
+                minValueZ = this.minZ + i * zCategorySize;
+                maxValueZ = minValueZ;
+                this.categories[i][j] = new Category(null, minValueX, maxValueX, minValueZ, maxValueZ);
+            }
+        }
+
     }
 }
