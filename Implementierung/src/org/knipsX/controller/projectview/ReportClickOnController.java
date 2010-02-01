f4811e92c1335d53e02b53d0acccc3e78af0c6f1
Kai Bouche <kai@bouche.at>
2010-02-01 23:45:11 +0100

    Doppelklick auch für Reports.
    Signed-off-by: Kai Bouche <kai@bouche.at>

---
+++ Implementierung/src/org/knipsX/controller/projectview/ReportClickOnController.java
@@ -0,0 +1,63 @@
+package org.knipsX.controller.projectview;
+
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.UIManager;
+
+import org.knipsX.controller.AbstractController;
+import org.knipsX.model.projectview.ProjectModel;
+import org.knipsX.model.reportmanagement.AbstractReportModel;
+import org.knipsX.view.projectview.JProjectView;
+import org.knipsX.view.reportmanagement.AbstractReportCompilation;
+import org.knipsX.view.reportmanagement.JReportConfig;
+
+/**
+ * Represents the Actions which are done by klicking on open report.
+ * Acts in harmony with a JProjectView.
+ */
+public class ReportClickOnController<M extends ProjectModel, V extends JProjectView<M>> extends
+        AbstractController<M, V> implements MouseListener {
+
+    private final int MOUSE_LEFT = 1;
+
+    public ReportClickOnController(M model, V view) {
+        super(model, view);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent mouseEvent) {
+        if (mouseEvent.getButton() == MOUSE_LEFT) {
+            if (mouseEvent.getClickCount() == 2) {
+                UIManager.put("TabbedPane.contentAreaColor", new Color(238, 238, 238));
+
+                if (this.view.getSelectedReports().length > 0) {
+                    new JReportConfig<AbstractReportModel, AbstractReportCompilation>((AbstractReportModel) this.model
+                            .getReports()[this.view.getSelectedReports()[0]], this.view.getSelectedReports()[0]);
+                }
+            }
+        }
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent arg0) {
+    }
+
+    @Override
+    public void mouseExited(MouseEvent arg0) {
+    }
+
+    @Override
+    public void mousePressed(MouseEvent arg0) {
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent arg0) {
+    }
+}
