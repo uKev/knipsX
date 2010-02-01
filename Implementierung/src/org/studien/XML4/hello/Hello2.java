6c82f036ffcfec05e02fdedabea7f068f7ce198a
clyde <clyde_swt@gmx.de>
2010-02-01 02:28:55 +0100

    XML::Unmashalling closes #94

--- Implementierung/src/org/studien/XML4/hello/Hello2.java
+++ Implementierung/src/org/studien/XML4/hello/Hello2.java
@@ -20,7 +20,35 @@ public void unmarshal() {
 		//u.setValidating(true);
        		//Object o = u.unmarshal( new File( "nosferatu.xml" ) );
 
-		Object collection = u.unmarshal(new File( "output.xml"));
+		//Object collection = u.unmarshal(new File( "output.xml"));
+
+		JAXBElement collection = (JAXBElement)u.unmarshal(new File( "output.xml"));
+			System.out.println("getValue(): " + collection.getValue() + "\n");
+			System.out.println("getName(): " + collection.getName() + "\n");
+			System.out.println("getScope(): " + collection.getScope() + "\n");
+			//System.out.println("size(): " + collection.size() + "\n");
+		//JAXBElement col2 = collection.getValue().createGreetings(); 
+		
+		//List grList = collection.getValue().getText();
+		//System.out.println("size():" + grList + "\n");
+
+	GreetingListType greetType = (hello.GreetingListType) collection.getValue();
+		List greetZ = greetType.getGreeting();
+		System.out.println("size: " + greetZ.size() + "\n");
+
+		for (int i= 0; i<greetZ.size();i++) {
+
+			System.out.println("Element: " + i + "\n");
+
+			hello.GreetingType greetRecord = (GreetingType) greetZ.get(i);
+
+			System.out.println("Text: " + greetRecord.getText() + "\n");
+			System.out.println("Language: " + greetRecord.getLanguage() + "\n");
+
+		}
+    //        List greetList = greetType.getBook();
+
+
 
 		//CollectionType.BooksType booksType = collection.getBooks();
             	//List bookList = booksType.getBook();
@@ -37,8 +65,13 @@ public void unmarshal() {
 
 		//if ( o != NULL ){
 		//	System.out.println("we have some data!!!");
+
+
+/*
 		List Greetz = new ArrayList(collection.getGreeting());
 		System.out.println("Nr.: " + Greetz.size()  + "\n");
+*/
+
 
 		//for( int i = 1; i < GreetList.size(); i++ ){
 	/*
@@ -52,11 +85,11 @@ public void unmarshal() {
                		System.out.println("Greeting's language: " +  Greeting.getLanguage() + "\n");
 
 			}
-*/
-		
+
+*/		
 	} catch( JAXBException jbe ) {
             // ...
         }
 
     }
- 
+} 
