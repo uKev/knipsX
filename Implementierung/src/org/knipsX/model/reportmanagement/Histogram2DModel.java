3042fcd152d4ca917e6ffc517a92691770ca5a68
Kevin Zuber <ukev@knet.eu>
2010-02-01 15:41:02 +0100

    Histogram2DModel reduced to minimum

--- Implementierung/src/org/knipsX/model/reportmanagement/Histogram2DModel.java
+++ Implementierung/src/org/knipsX/model/reportmanagement/Histogram2DModel.java
@@ -32,45 +32,4 @@ public class Histogram2DModel extends AbstractSingleAxisModel{
 		
 	}
 
-    @Override
-    public double getMaxX() {
-        // TODO Auto-generated method stub
-        return 0;
-    }
-
-    @Override
-    public double getMaxY() {
-        // TODO Auto-generated method stub
-        return 0;
-    }
-
-    @Override
-    public double getMinX() {
-        // TODO Auto-generated method stub
-        return 0;
-    }
-
-    @Override
-    public double getMinY() {
-        // TODO Auto-generated method stub
-        return 0;
-    }
-
-    @Override
-    public Axis getxAxis() {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-    @Override
-    public void setxAxis(Axis xAxis) {
-        // TODO Auto-generated method stub
-        
-    }
-
-    @Override
-    public ArrayList<PictureParameter> getPicturesWithMissingExifParameter() {
-        // TODO Auto-generated method stub
-        return null;
-    }
 }
