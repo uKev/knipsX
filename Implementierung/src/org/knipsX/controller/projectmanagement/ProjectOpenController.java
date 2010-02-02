3f454032ccda78036d9fad0b67e17209cf8fb0d4
BenniX47 <benni-borstel@online.de>
2010-02-02 11:27:03 +0100

    ProjectOpenController.java fertiggestellt und kommentiert.
    Signed-off-by: BenniX47 <benni-borstel@online.de>

--- Implementierung/src/org/knipsX/controller/projectmanagement/ProjectOpenController.java
+++ Implementierung/src/org/knipsX/controller/projectmanagement/ProjectOpenController.java
@@ -1,5 +1,9 @@
+/******************************************************************************
+ * This package is the root of all files regarding the "project management".
+ *****************************************************************************/
 package org.knipsX.controller.projectmanagement;
 
+/* import classes from java sdk */
 import java.awt.event.ActionEvent;
 import javax.swing.JOptionPane;
 import org.knipsX.controller.AbstractController;
@@ -10,22 +14,36 @@ import org.knipsX.view.projectview.JProjectView;
 
 /**
  * Represents the actions which are done by pushing the project open button.
- * 
  * Acts in harmony with JProjectManagement.
+ * 
+ * @param <M> The related model
+ * @param <V> The related view
  */
 public class ProjectOpenController<M extends ProjectManagementModel, V extends JProjectManagement<M>> extends
         AbstractController<M, V> {
 
+    /**
+     * Constructor for ProjectOpenController
+     * 
+     * @param model The related model
+     * @param view The related view
+     */
     public ProjectOpenController(M model, V view) {
         super(model, view);
     }
 
+    /**
+     * A project has to be selected before.
+     * 
+     * @see org.knipsX.controller.AbstractController#actionPerformed(java.awt.event.ActionEvent)
+     * @param event The action event
+     */
     @Override
-    public void actionPerformed(ActionEvent e) {
+    public void actionPerformed(ActionEvent event) {
 
         final int[] toOpen = this.view.getSelectedProjects();
 
-        /* only one project can copied at once */
+        /* only one project can opened */
         if (toOpen.length == 1) {
             ProjectModel projectModel = this.model.getProject(toOpen[0]);
             model.setStatus(ProjectManagementModel.INACTIVE);
@@ -33,12 +51,14 @@ public class ProjectOpenController<M extends ProjectManagementModel, V extends J
             new JProjectView<ProjectModel>(projectModel);
         } else if (toOpen.length == 0) {
 
-            /* gives the user a hint, that he has selected too little projects */
+            /* gives the user a hint, that he has selected no projects */
+            // INTERNATIONALIZE
             JOptionPane.showMessageDialog(null, "Selektieren Sie ein Projekt, um es zu öffnen.",
                     "Projekt öffnen - Fehler", JOptionPane.ERROR_MESSAGE);
         } else {
 
             /* gives the user a hint, that he has selected too much projects */
+            // INTERNATIONALIZE
             JOptionPane.showMessageDialog(null, "Selektieren Sie nur ein Projekt, um es zu öffnen.",
                     "Projekt öffnen - Fehler", JOptionPane.ERROR_MESSAGE);
 
