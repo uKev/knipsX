f87dc5ff8b969eec3bda4751b2cb4a9788b023de
BenniX47 <benni-borstel@online.de>
2010-02-02 10:12:43 +0100

    AbstractController.java implementiert und fertiggestellt. Closes #123
    Signed-off-by: BenniX47 <benni-borstel@online.de>

--- Implementierung/src/org/knipsX/controller/AbstractController.java
+++ Implementierung/src/org/knipsX/controller/AbstractController.java
@@ -1,21 +1,55 @@
+/******************************************************************************
+ * This package is the root of all controller classes.
+ *****************************************************************************/
 package org.knipsX.controller;
 
+/* import classes from the java sdk */
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+/**
+ * This AbstractController is the top of our controller structure. It represents ActionListener classes connected with
+ * views. They are getting userinteractions and evaluate them so they act in content.
+ * @param <M> The model
+ * @param <V> The view
+ */
 public abstract class AbstractController<M, V> implements ActionListener {
 
+    /* The related model */
     protected M model;
+    
+    /* The related view */
     protected V view;
 
+    /**
+     * Construct a Controller with a relation to the view
+     * 
+     * @param view
+     *            The realted view
+     */
     public AbstractController(final V view) {
-	this.view = view;
-    }
-    
-    public AbstractController(final M model, final V view) {
-	this.model = model;
-	this.view = view;
+        this.view = view;
     }
 
+    /**
+     * /**
+     * Construct a Controller with a relation to the model and the view
+     * 
+     * @param model
+     *            The related model
+     * @param view
+     *            The related view
+     */
+    public AbstractController(final M model, final V view) {
+        this.model = model;
+        this.view = view;
+    }
+
+    /**
+     * If a specific action is done by the user the related action is executed.
+     * 
+     * @param e
+     *            The Event happened before. For Example a button is pressed.
+     */
     public abstract void actionPerformed(ActionEvent e);
 }
