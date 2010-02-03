062aabafe8fce5a9fbd3242674ccd09426165f6f
clyde <clyde_swt@gmx.de>
2010-02-03 21:21:22 +0100

    XML update

--- Implementierung/src/org/knipsX/utils/JAXB/Handler.java
+++ Implementierung/src/org/knipsX/utils/JAXB/Handler.java
@@ -1,5 +1,38 @@
 package org.knipsX.utils.JAXB;
 
-public class Handler {
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+abstract class Handler {
+	protected static Map<Class<?>,Handler> ourClass2Conv = 
+	new HashMap<Class<?>,Handler>();
+
+	static {
+		ourClass2Conv.put( PersonType.class, new PersonHandler() );
+		ourClass2Conv.put( NameTypeclass, new NameHandler() );
+		ourClass2Conv.put( AddrType.class, new AddHandler() );
+		ourClass2Conv.put( ChildType.class, new Child Handler() );
+		//...
+
+	}
+	
+	public abstract void handle( Object o );
+
+	protected void process( Object obj ) {
+		if( obj != null ){
+			Handler h = ourClass2Conv.get( obj.getClass() );
+			if( h != null ){
+				h.handle( obj );
+			}
+
+		}
+	}
+
+	protected <T> void processList( List<T> list ){
+		for( T obj: list ) {
+			Handler h = this.getHandler( obj );
+			h.process( obj );
+		}
+	}
 }
