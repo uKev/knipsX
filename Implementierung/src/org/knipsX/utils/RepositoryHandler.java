6d763a3cd22af92d909cd8caf47071efa270291c
clyde <clyde_swt@gmx.de>
2010-02-04 11:03:20 +0100

    XML update

--- Implementierung/src/org/knipsX/utils/RepositoryHandler.java
+++ Implementierung/src/org/knipsX/utils/RepositoryHandler.java
@@ -9,18 +9,13 @@ public final class RepositoryHandler {
      */
     public static final String PROJECTS_PATH = System.getProperty("user.home") + File.separator + ".knipsX";
     
-<<<<<<< HEAD:Implementierung/src/org/knipsX/utils/RepositoryHandler.java
+    /*
+    * Set which repository should be used.
+    */
     private static Repository currentRepo = new XMLRepository();
     
-    //private static Repository currentRepo = new XMLRepository();
-=======
-    /*
-     * Set which repository should be used.
-     */
-    private static Repository currentRepo = new XMLRepositoryBackup();
     // private static Repository currentRepo = new XMLRepository();
     // private static Repository currentRepo = new DummyRepository();
->>>>>>> 0febe10767b0dc2d16e0bfb4726fa08b509ed4c7:Implementierung/src/org/knipsX/utils/RepositoryHandler.java
 
     public static Repository getRepository(){
         return RepositoryHandler.currentRepo;
