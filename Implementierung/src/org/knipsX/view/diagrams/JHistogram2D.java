1fc8e6bae26931bb2129512e105bcbd73efd4c78
Kevin Zuber <ukev@knet.eu>
2010-02-04 16:32:30 +0100

    debug output added

--- Implementierung/src/org/knipsX/view/diagrams/JHistogram2D.java
+++ Implementierung/src/org/knipsX/view/diagrams/JHistogram2D.java
@@ -6,6 +6,7 @@ import javax.swing.JOptionPane;
 import javax.vecmath.Vector3d;
 
 import org.apache.log4j.Logger;
+import org.knipsX.model.reportmanagement.Bar;
 import org.knipsX.model.reportmanagement.Category;
 import org.knipsX.model.reportmanagement.Histogram2DModel;
 
@@ -31,17 +32,16 @@ public class JHistogram2D<M extends Histogram2DModel> extends JAbstract2DDiagram
      */
     public JHistogram2D(final M model, final int reportID) {
         super(model, reportID);
-        JAbstract3DView.useBufferRange = false;        
+        JAbstract3DView.useBufferRange = false;
     }
 
     @Override
     public void generateContent() {
-        
+
         this.showGrid = false;
 
         if (model != null && this.model.isModelValid()) {
 
-            
             Logger logger = Logger.getLogger(this.getClass());
 
             logger.debug("Gloabl Min X " + this.model.getMinX() + " Global Max X " + this.model.getMaxX());
@@ -57,14 +57,15 @@ public class JHistogram2D<M extends Histogram2DModel> extends JAbstract2DDiagram
 
             int category = 0;
             double shrinkFactor = 0.85;
-            
+
             for (int i = 0; i < categories.length; i++) {
 
                 logger.debug("Category Number " + category + " Min X "
                         + this.getxAxis().getAxisSpace(categories[i].getMinValueX()) + " Max X "
-                        + this.getxAxis().getAxisSpace(categories[i].getMaxValueX()) + " Heigth of 1. Bar "
-                        + categories[i].getBars().get(0).getHeight() + "\n");
-
+                        + this.getxAxis().getAxisSpace(categories[i].getMaxValueX()) + " Heigth of Bar(s): ");
+                for (Bar bar : categories[i].getBars()) {
+                    logger.debug(bar.getHeight());
+                }
                 double xRange = Math.abs(this.getxAxis().getAxisSpace(categories[i].getMaxValueX())
                         - this.getxAxis().getAxisSpace(categories[i].getMinValueX()));
 
@@ -75,17 +76,18 @@ public class JHistogram2D<M extends Histogram2DModel> extends JAbstract2DDiagram
                 logger.debug("xPosition " + xPosition);
 
                 if (categories[i].getBars().get(0).getHeight() > 0) {
-                    
+
                     double barHeight = this.getyAxis().getAxisSpace(categories[i].getBars().get(0).getHeight());
-                    
+
                     /* Create the actual bar */
-                    this.createCube(new Vector3d(0, 0, xPosition), new Vector3d(xRange / 2 * shrinkFactor, barHeight, 1), this.basicMaterial(Color.orange));
-                    
+                    this.createCube(new Vector3d(0, 0, xPosition),
+                            new Vector3d(xRange / 2 * shrinkFactor, barHeight, 1), this.basicMaterial(Color.orange));
+
                     /* Create the actual number of elements on top of the bar */
                     double size = 0.33d;
-                    this.createText(new Vector3d(0, barHeight + 0.125 , xPosition), new Vector3d(size, size, size),
-                            this.basicMaterial(Color.white), Integer.toString((int) categories[i].getBars().get(
-                                    0).getHeight()));
+                    this.createText(new Vector3d(0, barHeight + 0.125, xPosition), new Vector3d(size, size, size), this
+                            .basicMaterial(Color.white), Integer.toString((int) categories[i].getBars().get(0)
+                            .getHeight()));
                 }
                 category++;
 
