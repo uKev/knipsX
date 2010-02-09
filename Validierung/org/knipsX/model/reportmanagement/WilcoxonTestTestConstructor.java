01e1f914758d09bff673ea04d06977df9b380ae8
BenniX47 <benni-borstel@online.de>
2010-02-10 00:09:51 +0100

    JUnit Tests erstellt.
    Signed-off-by: BenniX47 <benni-borstel@online.de>

---
+++ Validierung/org/knipsX/model/reportmanagement/WilcoxonTestTestConstructor.java
@@ -0,0 +1,56 @@
+/**
+ * 
+ */
+package org.knipsX.model.reportmanagement;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author Benni
+ *
+ */
+public class WilcoxonTestTestConstructor {
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+    }
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception {
+    }
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    /**
+     * Test method for {@link org.knipsX.model.reportmanagement.WilcoxonTest#WilcoxonTest()}.
+     */
+    @Test
+    public void testWilcoxonTest() {
+        fail("Not yet implemented");
+    }
+
+}
