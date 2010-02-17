b2219d4277d3c767df19240d56e975fa95d8ba1e
Kai Bouche <kai@bouche.at>
2010-02-17 14:43:00 +0100

    Updated the ProjectModel tests.

--- src/test/java/org/knipsX/model/projectview/ProjectModelTest.java
+++ src/test/java/org/knipsX/model/projectview/ProjectModelTest.java
@@ -2,218 +2,262 @@ package org.knipsX.model.projectview;
 
 import static org.junit.Assert.*;
 
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.LinkedList;
+import java.util.List;
+
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.knipsX.model.picturemanagement.PictureSet;
+import org.knipsX.model.reportmanagement.AbstractReportModel;
 
 public class ProjectModelTest {
 
-    @BeforeClass
-    public static void setUpBeforeClass() throws Exception {
-    }
+    private int testId = 0;
 
-    @AfterClass
-    public static void tearDownAfterClass() throws Exception {
-    }
+    private String testName = "test name";
+    private String testDescription = "test description";
+
+    private GregorianCalendar testCalendar = new GregorianCalendar(2009, 1, 1, 1, 2, 3);
+
+    private List<PictureSet> testPictureSets;
+    private List<AbstractReportModel> testReports;
+
+    private ProjectModel testModel;
 
     @Before
     public void setUp() throws Exception {
+        this.testPictureSets = new LinkedList<PictureSet>();
+        this.testReports = new LinkedList<AbstractReportModel>();
+
+        this.testModel = new ProjectModel(this.testId, this.testName, this.testDescription, this.testCalendar,
+                this.testPictureSets, this.testReports);
     }
 
-    @After
-    public void tearDown() throws Exception {
-    }
+    /*
+     * #######################################
+     * -GETTER TEST-
+     * #######################################
+     */
 
-    @Test
-    public void testUpdateViews() {
-        fail("Not yet implemented");
-    }
-
-    @Test
-    public void testProjectModelIntStringStringGregorianCalendarListOfPictureSetListOfAbstractReportModel() {
-        fail("Not yet implemented");
-    }
-
-    @Test
-    public void testProjectModelProjectModelIntString() {
-        fail("Not yet implemented");
-    }
-
-    @Test
-    public void testSetStatus() {
-        fail("Not yet implemented");
-    }
-
-    @Test
-    public void testGetStatus() {
-        fail("Not yet implemented");
-    }
-
-    @Test
-    public void testGetCreationDate() {
-        fail("Not yet implemented");
-    }
+    /*
+     * #######################################
+     * PRIMITIV GETTER TEST
+     * #######################################
+     */
 
     @Test
     public void testGetId() {
-        fail("Not yet implemented");
+        assertEquals("Expected identifier wasn't setup correct.", this.testId, this.testModel.getId());
     }
 
     @Test
     public void testGetName() {
-        fail("Not yet implemented");
-    }
-
-    @Test
-    public void testSetName() {
-        fail("Not yet implemented");
+        assertEquals("Expected name wasn't setup correct.", this.testName, this.testModel.getName());
     }
 
     @Test
     public void testGetDescription() {
-        fail("Not yet implemented");
+        assertEquals("Expected description wasn't setup correct.", this.testDescription, this.testModel
+                .getDescription());
     }
 
     @Test
-    public void testSetProjectDescription() {
-        fail("Not yet implemented");
-    }
-
-    @Test
-    public void testGetExifParameter() {
-        fail("Not yet implemented");
+    public void testGetCreationDate() {
+        assertEquals("Expected creation date wasn't setup correct.", this.testCalendar, this.testModel
+                .getCreationDate());
     }
 
     @Test
     public void testCalendarToString() {
-        fail("Not yet implemented");
+        
+        /* Must be the same as above! */
+        assertEquals("Expected string representation of the creation date didn't match.", "1.2.2009 - 01:02:03",
+                this.testModel.calendarToString());
     }
 
     @Test
-    public void testGetNumberOfPictures() {
-        fail("Not yet implemented");
+    public void testGetStatus() {
+        assertEquals("Expected initial project status wasn't setup correct", ProjectModel.ACTIVE, this.testModel
+                .getStatus());
     }
 
-    @Test
-    public void testGetNumberOfPicturesWithoutExifData() {
-        fail("Not yet implemented");
-    }
+    /*
+     * #######################################
+     * COMPLEX GETTER TEST
+     * #######################################
+     */
 
-    @Test
-    public void testGetNumberOfPicturesWithoutThumbails() {
-        fail("Not yet implemented");
-    }
+//    @Test
+//    public void testGetExifParameter() {
+//        fail("Not yet implemented");
+//    }
+//
+//    @Test
+//    public void testGetNumberOfPictures() {
+//        fail("Not yet implemented");
+//    }
+//
+//    @Test
+//    public void testGetNumberOfPicturesWithoutExifData() {
+//        fail("Not yet implemented");
+//    }
+//
+//    @Test
+//    public void testGetNumberOfPicturesWithoutThumbails() {
+//        fail("Not yet implemented");
+//    }
+//
+//    @Test
+//    public void testGetPictureSets() {
+//        fail("Not yet implemented");
+//    }
+//
+//    @Test
+//    public void testGetSelectedPictureSet() {
+//        fail("Not yet implemented");
+//    }
+//
+//    @Test
+//    public void testSetSelectedPictureSet() {
+//        fail("Not yet implemented");
+//    }
+//
+//    @Test
+//    public void testGetPictureSetsFromPictureSet() {
+//        fail("Not yet implemented");
+//    }
+//
+//    @Test
+//    public void testGetDirectoriesFromPictureSet() {
+//        fail("Not yet implemented");
+//    }
+//
+//    @Test
+//    public void testGetPicturesFromPictureSet() {
+//        fail("Not yet implemented");
+//    }
+//
+//    @Test
+//    public void testGetSelectedPictureSetContent() {
+//        fail("Not yet implemented");
+//    }
+//
+//    @Test
+//    public void testGetAllPictures() {
+//        fail("Not yet implemented");
+//    }
+//
+//    @Test
+//    public void testGetPicturesOfADirectory() {
+//        fail("Not yet implemented");
+//    }
+//
+//    @Test
+//    public void testGetSelectedPicture() {
+//        fail("Not yet implemented");
+//    }
+//
+//    @Test
+//    public void testGetReports() {
+//        fail("Not yet implemented");
+//    }
 
-    @Test
-    public void testLoadData() {
-        fail("Not yet implemented");
-    }
+    /*
+     * #######################################
+     * -SETTER TEST-
+     * #######################################
+     */
 
-    @Test
-    public void testSaveProjectModel() {
-        fail("Not yet implemented");
-    }
+    /*
+     * #######################################
+     * PRIMITIV SETTER TEST
+     * #######################################
+     */
 
-    @Test
-    public void testAddPictureSet() {
-        fail("Not yet implemented");
-    }
+//    @Test
+//    public void testSetName() {
+//        fail("Not yet implemented");
+//    }
+//
+//    @Test
+//    public void testSetDescription() {
+//        fail("Not yet implemented");
+//    }
+//
+//    @Test
+//    public void testSetStatus() {
+//        fail("Not yet implemented");
+//    }
+//
+//    @Test
+//    public void testSetSelectedPicture() {
+//        fail("Not yet implemented");
+//    }
+//
+//    @Test
+//    public void testSetSelectedPictureSetContent() {
+//        fail("Not yet implemented");
+//    }
 
-    @Test
-    public void testRemovePictureSet() {
-        fail("Not yet implemented");
-    }
+    /*
+     * #######################################
+     * -OTHER TEST-
+     * #######################################
+     */
 
-    @Test
-    public void testGetPictureSets() {
-        fail("Not yet implemented");
-    }
+//    @Test
+//    public void testLoadData() {
+//        fail("Not yet implemented");
+//    }
+//
+//    @Test
+//    public void testSaveProjectModel() {
+//        fail("Not yet implemented");
+//    }
 
-    @Test
-    public void testGetSelectedPictureSet() {
-        fail("Not yet implemented");
-    }
+    /*
+     * #######################################
+     * -ADD/REMOVE/REFRESH STUFF TEST-
+     * #######################################
+     */
 
-    @Test
-    public void testSetSelectedPictureSet() {
-        fail("Not yet implemented");
-    }
-
-    @Test
-    public void testGetPictureSetsFromPictureSet() {
-        fail("Not yet implemented");
-    }
-
-    @Test
-    public void testGetDirectoriesFromPictureSet() {
-        fail("Not yet implemented");
-    }
-
-    @Test
-    public void testGetPicturesFromPictureSet() {
-        fail("Not yet implemented");
-    }
-
-    @Test
-    public void testAddContentToPictureSet() {
-        fail("Not yet implemented");
-    }
-
-    @Test
-    public void testRemoveContentFromPictureSet() {
-        fail("Not yet implemented");
-    }
-
-    @Test
-    public void testGetSelectedPictureSetContent() {
-        fail("Not yet implemented");
-    }
-
-    @Test
-    public void testSetSelectedPictureSetContent() {
-        fail("Not yet implemented");
-    }
-
-    @Test
-    public void testRefreshAllDirectories() {
-        fail("Not yet implemented");
-    }
-
-    @Test
-    public void testGetAllPictures() {
-        fail("Not yet implemented");
-    }
-
-    @Test
-    public void testGetPicturesOfADirectory() {
-        fail("Not yet implemented");
-    }
-
-    @Test
-    public void testGetSelectedPicture() {
-        fail("Not yet implemented");
-    }
-
-    @Test
-    public void testSetSelectedPicture() {
-        fail("Not yet implemented");
-    }
-
-    @Test
-    public void testAddReport() {
-        fail("Not yet implemented");
-    }
-
-    @Test
-    public void testRemoveReport() {
-        fail("Not yet implemented");
-    }
-
-    @Test
-    public void testGetReports() {
-        fail("Not yet implemented");
-    }
-
+//    @Test
+//    public void testRefreshAllDirectories() {
+//        fail("Not yet implemented");
+//    }
+//
+//    @Test
+//    public void testAddPictureSet() {
+//        fail("Not yet implemented");
+//    }
+//
+//    @Test
+//    public void testAddContentToPictureSet() {
+//        fail("Not yet implemented");
+//    }
+//
+//    @Test
+//    public void testAddReport() {
+//        fail("Not yet implemented");
+//    }
+//
+//    @Test
+//    public void testRemovePictureSet() {
+//        fail("Not yet implemented");
+//    }
+//
+//    @Test
+//    public void testRemoveContentFromPictureSet() {
+//        fail("Not yet implemented");
+//    }
+//
+//    @Test
+//    public void testRemoveReport() {
+//        fail("Not yet implemented");
+//    }
 }
