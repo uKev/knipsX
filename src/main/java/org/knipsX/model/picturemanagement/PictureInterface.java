8cb832c21f1368e788e85907eaad0ec0722128d9
Kevin Zuber <ukev@knet.eu>
2010-03-10 11:23:09 +0100

    Made gestScaledInstance private

--- src/main/java/org/knipsX/model/picturemanagement/PictureInterface.java
+++ src/main/java/org/knipsX/model/picturemanagement/PictureInterface.java
@@ -142,34 +142,6 @@ public interface PictureInterface extends PictureContainer {
     Object[][] getAllExifParameter();
 
     /**
-     * Convenience method that returns a scaled instance of the
-     * provided {@code BufferedImage}.
-     * 
-     * @param img
-     *            the original image to be scaled
-     * @param targetWidth
-     *            the desired width of the scaled instance,
-     *            in pixels
-     * @param targetHeight
-     *            the desired height of the scaled instance,
-     *            in pixels
-     * @param hint
-     *            one of the rendering hints that corresponds to {@code RenderingHints.KEY_INTERPOLATION} (e.g. {@code
-     *            RenderingHints.VALUE_INTERPOLATION_NEAREST_NEIGHBOR}, {@code
-     *            RenderingHints.VALUE_INTERPOLATION_BILINEAR}, {@code RenderingHints.VALUE_INTERPOLATION_BICUBIC})
-     * @param higherQuality
-     *            if true, this method will use a multi-step
-     *            scaling technique that provides higher quality than the usual
-     *            one-step technique (only useful in downscaling cases, where {@code targetWidth} or {@code
-     *            targetHeight} is
-     *            smaller than the original dimensions, and generally only when
-     *            the {@code BILINEAR} hint is specified)
-     * @return a scaled version of the original {@code BufferedImage}
-     */
-    BufferedImage getScaledInstance(final BufferedImage img, final int targetWidth,
-            final int targetHeight, final Object hint, final boolean higherQuality);
-
-    /**
      * It also allows to compare over PictureContainer but it is not done in the basic version of our programm.
      * 
      * @see java.lang.Comparable#compareTo(java.lang.Object)
