a2147ae9514d1b9ba2baa555fd7bbfa9a7834521
David Kaufman <david.kaufman@gmx.de>
2010-03-11 21:15:58 +0100

    Removed a unused import

--- src/main/java/org/knipsX/view/diagrams/JAbstract3DView.java
+++ src/main/java/org/knipsX/view/diagrams/JAbstract3DView.java
@@ -14,7 +14,6 @@ import java.awt.Point;
 import java.awt.image.BufferedImage;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
