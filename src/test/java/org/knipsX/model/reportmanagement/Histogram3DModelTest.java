4c26c7bb465a10dcaaf9d8711d9920da772083de
Kevin Zuber <uKev@knet.eu>
2010-03-12 01:08:45 +0100

    Fixed TestCase

--- src/test/java/org/knipsX/model/reportmanagement/Histogram3DModelTest.java
+++ src/test/java/org/knipsX/model/reportmanagement/Histogram3DModelTest.java
@@ -6,11 +6,14 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.knipsX.images.dummypictures.DummyPictures;
+import org.knipsX.utils.ExifParameter;
+import org.knipsX.utils.UtilsForTesting;
 
 public class Histogram3DModelTest {
 
     @BeforeClass
     public static void setUpBeforeClass() throws Exception {
+        UtilsForTesting.initLogger();
     }
 
     @Before
@@ -27,8 +30,10 @@ public class Histogram3DModelTest {
         
         Histogram3DModel testModel = new Histogram3DModel();
         testModel.addPictureContainer(DummyPictures.getDummyPictureSet());
+        testModel.setXAxis(new Axis(ExifParameter.FOCALLENGTH));
+        testModel.setZAxis(new Axis(ExifParameter.FNUMBER));
         
-        assertTrue(testModel.isModelValid());
+        assertTrue("Model is not valid", testModel.isModelValid());
         
         
     }
